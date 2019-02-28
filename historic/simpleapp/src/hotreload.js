8237e04c6d642c4b0ca818b51b0f3422ac0b6c7a
Vanessa Freudenberg <vanessa@codefrau.net>
2019-02-27 18:21:04 -0800

    simpleapp: add generic hotreload dispose handlers

--- historic/simpleapp/src/hotreload.js
+++ historic/simpleapp/src/hotreload.js
@@ -1,6 +1,7 @@
 let timeoutHandles = new Set();
 let frameHandles = new Set();
 let eventListeners = [];
+let disposeHandlers = [];
 
 function setTimeout(fn, ms) {
     const handle = window.setTimeout((...args) => {
@@ -25,19 +26,26 @@ function addEventListener(obj, ...args) {
     return obj.addEventListener(...args);
 }
 
+function addDisposeHandler(fn) {
+    disposeHandlers.push(fn);
+}
+
 function dispose() {
     for (let handle of timeoutHandles) window.clearTimeout(handle);
     for (let handle of frameHandles) window.cancelAnimationFrame(handle);
     for (let {obj, args} of eventListeners) obj.removeEventListener(...args);
-    console.log(`Clearing callbacks: ${timeoutHandles.size} timeouts, ${frameHandles.size} animationFrames, ${eventListeners.length} eventListeners`);
+    for (let fn of disposeHandlers) fn();
+    console.log(`Clearing ${timeoutHandles.size} timeouts, ${frameHandles.size} animationFrames, ${eventListeners.length} eventListeners, ${disposeHandlers.length} disposeHandlers`);
     timeoutHandles = new Set();
     frameHandles = new Set();
     eventListeners = [];
+    disposeHandlers = [];
 }
 
 export default {
     setTimeout,
     requestAnimationFrame,
     addEventListener,
+    addDisposeHandler,
     dispose,
 };
