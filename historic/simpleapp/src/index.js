e3c33facbd62c6b3f66da1b93584a83891f2da2c
Vanessa Freudenberg <vanessa@codefrau.net>
2019-03-24 15:24:48 -0700

    croquetkit: lint

--- historic/simpleapp/src/index.js
+++ historic/simpleapp/src/index.js
@@ -107,8 +107,8 @@ function start() {
             renderer.render(currentRoomName, ALL_ROOMS, roomViewManager);
             Stats.end("render");
 
-            const island = ALL_ROOMS[currentRoomName].island;
-            Stats.users(island ? island.controller.users : 0);
+            const currentIsland = ALL_ROOMS[currentRoomName].island;
+            Stats.users(currentIsland ? currentIsland.controller.users : 0);
 
             const currentRoomView = roomViewManager.getIfLoaded(currentRoomName);
             if (currentRoomView) {
