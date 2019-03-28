5c4de23eda1986c3c7b4e34a1628a0813826f15e
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2019-03-28 11:12:15 -0700

    support shift key. move some parameters around

--- historic/simpleapp/src/sampleRooms/room3.js
+++ historic/simpleapp/src/sampleRooms/room3.js
@@ -10,24 +10,28 @@ function initRoom3(state) {
         const room = new Room();
         const text1 = new Editor({
             spatial: { position: {x: -3, y: 1, z: 0} },
-            editableText: {content: {
-                content: [{text: "man is much more than a tool builder... he is an inventor of universes... Except the real one."}],
-                selection: {start: 0, end: 0}}},
-            font: "Roboto", numLines: 10, width: 3, height: 2},
-        {
-            editable: true,
-        });
+            editableText: {
+                content: {
+                    content: [{text: "man is much more than a tool builder... he is an inventor of universes... Except the real one."}],
+                    selection: {start: 0, end: 0}},
+                }},
+            {
+                editable: true,
+                font: "Roboto", numLines: 10, width: 3, height: 2
+            });
         room.parts.objects.add(text1);
 
         const text2 = new Editor({
             spatial: { position: {x: 3, y: 1, z: 0} },
-            editableText: {content: {
-                content: [{text: "man is much more than a tool builder... he is an inventor of universes..."}],
-                selection: {start: 0, end: 0}}},
-            font: "Barlow", numLines: 10, width: 3, height: 2},
-        {
-            editable: true,
-        });
+            editableText: {
+                content: {
+                    content: [{text: "man is much more than a tool builder... he is an inventor of universes..."}],
+                    selection: {start: 0, end: 0}},
+                }},
+            {
+                editable: true,
+                font: "Barlow", numLines: 10, width: 3, height: 2
+            });
         room.parts.objects.add(text2);
     });
 }
