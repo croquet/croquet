404c15f97ac095b89006e3dec7f1ee72da94e705
Vanessa Freudenberg <vanessa@codefrau.net>
2019-04-03 13:41:53 -0700

    teatime-replay: combined code is generated, but hashes are wrong

--- historic/simpleapp/src/replay.js
+++ historic/simpleapp/src/replay.js
@@ -2,6 +2,14 @@
 //import { server } from "reflector";
 
 const iframe = document.getElementsByTagName("iframe")[0];
+const textarea = document.getElementsByTagName("textarea")[0];
+
+function log(...args) {
+    console.log(...args);
+    textarea.value += args.join(' ') + '\n';
+    textarea.scrollTop = textarea.scrollHeight;
+}
+
 
 const snapshotURL = window.location.hash.slice(1);
 //const indexURL = `${window.location.protocol}//${window.location.host}/index.html`;
@@ -17,39 +25,73 @@ if (snapshotURL) {
 }
 
 async function start() {
+    log(`fetching snapshot ${snapshotURL}`);
     const response = await fetch(snapshotURL, { mode: "cors" });
     const snapshot = await response.json();
-    const filesToLoad = await loadCode(snapshot.meta.code);
-    for (const file of filesToLoad) {
-        console.log(file.name, file.generated.js.length, file.id, file.deps);
+    const {prelude, files, html} = await loadCode(snapshot.meta.code);
+    const {entry} = snapshot.meta.code;
+
+    // the Parcel prelude defines the main function
+    //     parcelRequire = function (modules, cache, entry, globalName) {...}
+    // which we want to call as
+    //     parcelRequire(modules, {}, [entry], null)
+    // where modules is the definition of all modules:
+    //     modules = {
+    //         id: [
+    //             function(require,module,exports) { ...source... },
+    //             { ... dependencies ...}
+    //         ],
+    //     }
+
+    const modules = [];
+
+    for (const {name, hash, imports, source} of files) {
+        const deps = Object.entries(imports||[]).map(([k,v]) => `"${k}":"${v}"`);
+        modules.push(`"${hash}":/*${name}*/[function(require,module,exports) {\n \n}, {${deps.join(',')}}]`);
     }
+debugger
+    const combinedSources = `debugger;${prelude}\nparcelRequire({\n${modules.join(',\n')}\n}, {}, ["${entry}"], null)`
+        .split("<!--").join("<\\!--")
+        .split("<script").join("<\\script")
+        .split("</script").join("<\\/script");
+
+    const combinedHtml = html.replace('<script src="entry.js"></script>', `<script>${combinedSources}</script>`);
+
+    fetch('https://db.croquet.studio/files-v1/bert-test/test.html', {
+            method: "PUT",
+            mode: "cors",
+            body: combinedHtml,
+        });
+
+    iframe.contentWindow.document.open();
+    iframe.contentWindow.document.write(combinedHtml);
+    iframe.contentWindow.document.close();
 }
 
 
 async function loadCode(stored) {
-    const { base, entry } = stored;
+    const { base, prelude, entry, html } = stored;
     const filesToLoad = [];
     const filesToCheck = [entry];
     const checkedFiles = {};
 
+    log(`loading code from ${base} ...`);
+
     // eslint-disable-next-line no-await-in-loop
     while (filesToCheck.length) await checkFile(filesToCheck.shift());
+
+    async function fetchFile(file, ext) { return fetch(`${base}${file}.${ext}`).then(r => r[ext === 'json' ? 'json' : 'text']()); }
 
     async function checkFile(hash) {
         if (checkedFiles[hash]) return;
         checkedFiles[hash] = true;
-        const meta = await fetch(`${base}${hash}.json`).then(r => r.json());
+        const meta = await fetchFile(hash, 'json');
         const {name, imports} = meta;
-        filesToLoad.push({
-            name,
-            id: hash,
-            type: "js",
-            deps: imports,
-            generated: fetch(`${base}${hash}.js`).then(r => r.text()),
-        });
+        log(`fetching ${name} from ${hash}.js`);
+        filesToLoad.push({ name, hash, imports, source: fetchFile(hash, 'js') });
         await Promise.all(Object.values(imports||[]).map(checkFile));
     }
 
-    await Promise.all(filesToLoad.map(async file => file.generated = { js: await file.generated }));
-    return filesToLoad;
+    await Promise.all(filesToLoad.map(async file => file.source = await file.source ));
+    return {prelude: await fetchFile(prelude, 'js'), files: filesToLoad, html: await fetchFile(html, 'html')};
 }
