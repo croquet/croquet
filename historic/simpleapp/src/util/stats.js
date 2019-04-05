4c0727acf5b8c5fe6b04b0fe3f23b3d8ace492e1
Vanessa Freudenberg <vanessa@codefrau.net>
2019-04-05 13:00:30 -0700

    stats: make stat functions no-ops if disabled

--- historic/simpleapp/src/util/stats.js
+++ historic/simpleapp/src/util/stats.js
@@ -62,10 +62,97 @@ function newFrame(now) {
     };
 }
 
+function endCurrentFrame(timestamp) {
+    // add current frame to end
+    currentFrame.total = timestamp - currentFrame.start;
+    frames.push(currentFrame);
+
+    // get base framerate as minimum of all frames
+    const realFrames = frames.slice(1).filter(f => f.total);
+    const avgMS = realFrames.map(f => f.total).reduce( (a,b) => a + b, 0) / realFrames.length;
+    const newMax = Math.max(...realFrames.map(f => Math.max(f.backlog, f.network)));
+    maxBacklog = Math.max(newMax, maxBacklog * 0.98); // reduce scale slowly
+
+    while (frames.length > 120) frames.shift();
+
+    // show average framerate
+    fps.innerText = `${currentFrame.users} users, ${Math.round(1000/avgMS)} fps,
+        backlog: ${currentFrame.backlog < 100 ? '0.0' : (currentFrame.backlog/1000).toFixed(1)} s`;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const oneFrame = 1000 / 60;
+    const map = v => (1 - v / oneFrame) * 20 + 60;
+    const mapBacklog = v => map(v / Math.max(3000, maxBacklog) * -200) + 5;
+    for (let i = 0; i < frames.length; i++) {
+        const frame = frames[i];
+        const x = i + 0.5;
+        let y = map(0);
+
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x, map(frame.total));
+        ctx.strokeStyle = colors[frame.connected ? "total" : "network"];
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        y = map(frame.total);
+        let ms = 0;
+        for (const item of order) {
+            if (!frame.items[item]) continue;
+            ms += frame.items[item];
+            y = map(ms);
+            ctx.lineTo(x, y);
+            ctx.strokeStyle = colors[item];
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+        }
+
+        if (frame.network) {
+            ctx.moveTo(x, mapBacklog(0));
+            ctx.lineTo(x, mapBacklog(frame.network));
+            ctx.strokeStyle = colors["network"];
+            ctx.stroke();
+        }
+        if (frame.backlog) {
+            ctx.moveTo(x, mapBacklog(0));
+            ctx.lineTo(x, mapBacklog(frame.backlog));
+            ctx.strokeStyle = colors["backlog"];
+            ctx.stroke();
+        }
+    }
+    // draw horizontal lines over graph, one per frame
+    ctx.strokeStyle = 'rgba(255, 255, 255, 0.2)';
+    for (let y = 0; y < 60; y += oneFrame) {
+        ctx.moveTo(0, map(y));
+        ctx.lineTo(120, map(y));
+        ctx.stroke();
+    }
+
+    if (maxBacklog > 500) {
+        // draw lines with labels for backlog
+        // use log10 to draw lines every 1s, or 10s, or 100s etc.
+        const unit = 10 ** Math.floor(Math.log10(Math.max(3000, maxBacklog)));
+        ctx.font = '10pt sans-serif';
+        ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+        for (let i = 1; i < 11; i++) {
+            const value = i * unit;
+            const y = mapBacklog(value);
+            ctx.moveTo(0, y);
+            ctx.lineTo(120, y);
+            ctx.stroke();
+            ctx.fillText(`${value / 1000}s`, 0, y - 5);
+            if (value > maxBacklog) break;
+        }
+    }
+
+    div.style.bottom = mapBacklog(Math.max(1000, maxBacklog)) - 350;
+}
+
 const Stats = {
     animationFrame(timestamp) {
-        if (!div) return;
-        this.endCurrentFrame(timestamp);
+        endCurrentFrame(timestamp);
         currentFrame = newFrame(timestamp);
     },
     begin(item) {
@@ -88,93 +175,12 @@ const Stats = {
     connected(bool) {
         currentFrame.connected = connected = bool;
     },
-    endCurrentFrame(timestamp) {
-        // add current frame to end
-        currentFrame.total = timestamp - currentFrame.start;
-        frames.push(currentFrame);
-
-        // get base framerate as minimum of all frames
-        const realFrames = frames.slice(1).filter(f => f.total);
-        const avgMS = realFrames.map(f => f.total).reduce( (a,b) => a + b, 0) / realFrames.length;
-        const newMax = Math.max(...realFrames.map(f => Math.max(f.backlog, f.network)));
-        maxBacklog = Math.max(newMax, maxBacklog * 0.98); // reduce scale slowly
-
-        while (frames.length > 120) frames.shift();
-
-        // show average framerate
-        fps.innerText = `${currentFrame.users} users, ${Math.round(1000/avgMS)} fps,
-            backlog: ${currentFrame.backlog < 100 ? '0.0' : (currentFrame.backlog/1000).toFixed(1)} s`;
-
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        const oneFrame = 1000 / 60;
-        const map = v => (1 - v / oneFrame) * 20 + 60;
-        const mapBacklog = v => map(v / Math.max(3000, maxBacklog) * -200) + 5;
-        for (let i = 0; i < frames.length; i++) {
-            const frame = frames[i];
-            const x = i + 0.5;
-            let y = map(0);
-
-            ctx.beginPath();
-            ctx.moveTo(x, y);
-            ctx.lineTo(x, map(frame.total));
-            ctx.strokeStyle = colors[frame.connected ? "total" : "network"];
-            ctx.stroke();
-
-            ctx.beginPath();
-            ctx.moveTo(x, y);
-            y = map(frame.total);
-            let ms = 0;
-            for (const item of order) {
-                if (!frame.items[item]) continue;
-                ms += frame.items[item];
-                y = map(ms);
-                ctx.lineTo(x, y);
-                ctx.strokeStyle = colors[item];
-                ctx.stroke();
-                ctx.beginPath();
-                ctx.moveTo(x, y);
-            }
-
-            if (frame.network) {
-                ctx.moveTo(x, mapBacklog(0));
-                ctx.lineTo(x, mapBacklog(frame.network));
-                ctx.strokeStyle = colors["network"];
-                ctx.stroke();
-            }
-            if (frame.backlog) {
-                ctx.moveTo(x, mapBacklog(0));
-                ctx.lineTo(x, mapBacklog(frame.backlog));
-                ctx.strokeStyle = colors["backlog"];
-                ctx.stroke();
-            }
-        }
-        // draw horizontal lines over graph, one per frame
-        ctx.strokeStyle = 'rgba(255, 255, 255, 0.2)';
-        for (let y = 0; y < 60; y += oneFrame) {
-            ctx.moveTo(0, map(y));
-            ctx.lineTo(120, map(y));
-            ctx.stroke();
-        }
-
-        if (maxBacklog > 500) {
-            // draw lines with labels for backlog
-            // use log10 to draw lines every 1s, or 10s, or 100s etc.
-            const unit = 10 ** Math.floor(Math.log10(Math.max(3000, maxBacklog)));
-            ctx.font = '10pt sans-serif';
-            ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-            for (let i = 1; i < 11; i++) {
-                const value = i * unit;
-                const y = mapBacklog(value);
-                ctx.moveTo(0, y);
-                ctx.lineTo(120, y);
-                ctx.stroke();
-                ctx.fillText(`${value / 1000}s`, 0, y - 5);
-                if (value > maxBacklog) break;
-            }
-        }
-
-        div.style.bottom = mapBacklog(Math.max(1000, maxBacklog)) - 350;
-    }
 };
 
-export default Stats;
+const NoStats = {};
+const Noop = () => {};
+for (const key of (Object.keys(Stats))) {
+    NoStats[key] = Noop;
+}
+
+export default div ? Stats : NoStats;
