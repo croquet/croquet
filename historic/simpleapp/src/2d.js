1f3a7ad4f1e4cdaa543738162351ab0c7ea80f6c
Vanessa Freudenberg <vanessa@codefrau.net>
2019-04-04 18:03:11 -0700

    teatime: 2d HTML demo is starting to work

--- historic/simpleapp/src/2d.js
+++ historic/simpleapp/src/2d.js
@@ -1,126 +1,139 @@
 import Island, { connectToReflector, Controller, addMessageTranscoder } from "./island.js";
 import Model from "./model.js";
 import View from "./view.js";
+import Stats from "./util/stats.js";
 
+const THROTTLE = 1000 / 60;
 
-const COLORS = [ "white", "lightblue", "yellow", "lightgreen" ];
-
+let SCALE = 1; // model uses virtual 1000x1000 space
 
 addMessageTranscoder('*', a => a, a => a);
 
-
-export class Text extends Model {
+export class Root extends Model {
 
     constructor(state) {
         super(state);
-        this.island.set('text', this);
-        this.text = state.text || {
-            contents: 'Type something!',
-            selections: {},
-        };
+        this.island.set('root', this);
+        this.children = [];
+    }
+
+    restoreObjectReferences(state, objectsByID) {
+        this.children = state.children.map(id => objectsByID[id]);
     }
 
     toState(state) {
         super.toState(state);
-        state.text = this.text;
+        state.children = this.children.map(child => child.id);
     }
 
     // non-inherited methods below
 
-    changed() {
-        this.publish(this.id, null, 'text-changed', this.text);
-        console.log(JSON.stringify(this.text));
+    add(child) {
+        this.children.push(child);
+        this.publish(this.id, null, 'child-added', child.id);
     }
 
-    keydown(user, key, shift, cmd) {
-        const sel = this.text.selections[user];
-        this.replace(sel, key);
-    }
-
-    replace(sel, string) {
-        const text = this.text.contents;
-        this.text.contents = text.slice(0, sel.start) + string + text.slice(sel.end);
-        const delta = sel.end - sel.start + string.length;
-        for (const each of Object.values(this.text.selections)) {
-            if ((sel !== each) && each.start > sel.end) {
-                each.start += delta;
-                each.end += delta;
-            }
-        }
-        sel.end = sel.start + string.length;
-        sel.start = sel.end;
-        this.changed();
-    }
-
-    createSelection(user) {
-        if (!this.text.selections[user]) {
-            this.text.selections[user] = { start: 0, end: 0, color: this.unusedColor() };
-        }
-        this.changed();
-    }
-
-    unusedColor() {
-        const usedColors = Object.values(this.text.selections).map(sel => sel.color).sort();
-        let i = 0;
-        while (usedColors.includes(i)) i++;
-        return i;
-    }
 }
 
 
-class TextView extends View {
+export class Shape extends Model {
 
-    attach(modelState) {
-        super.attach(modelState);
-        this.div = document.createElement("div");
-        this.render(modelState.text);
-        document.body.appendChild(this.div);
+    constructor(state={}) {
+        super(state);
+        this.island.set('text', this);
+        const r = max => Math.floor(max * this.island.random());
+        this.type = state.type || 'circle';
+        this.color = state.color || `hsl(${r(360)},${r(50)+50}%,50%)`;
+        this.pos = state.pos || [r(1000), r(1000)];
+    }
 
-        this.subscribePart(this.modelId, null, 'text-changed', null, "render", true);
-
-        document.addEventListener("keydown", evt => this.model.keydown(this.user, evt.key, evt.shiftKey, evt.metaKey || evt.ctrlKey));
-
-        this.user = localStorage.user || (localStorage.user = window.prompt("Who are you?"));
-        this.model.createSelection(this.user);
+    toState(state) {
+        super.toState(state);
+        state.type = this.type;
+        state.color = this.color;
+        state.pos = this.pos;
     }
 
     // non-inherited methods below
 
-    render(text) {
-        // clear old spans
-        while (this.div.firstChild) this.div.removeChild(this.div.firstChild);
-        // create characters
-        const chars = text.contents.split('').map(char => ({char, color: 0}));
-        // apply selections
-        for (const { start, end, color } of Object.values(text.selections)) {
-            for (let i = start; i <= end; i++) {
-                chars[i].color |= 1 << color;
-            }
-        }
-        chars.push({char: '', color: -1}); // force end of last span
-        // create spans
-        let prev = 0;
-        let string = '';
-        for (const {char, color} of Object.values(chars)) {
-            if (color !== prev && string) {
-                const span = document.createElement("span");
-                span.style.backgroundColor = COLORS[prev % COLORS.length];
-                span.innerText = string;
-                this.div.appendChild(span);
-                string = '';
-            }
-            prev = color;
-            string += char;
-        }
+    moveBy(dx, dy) {
+        this.pos[0] = Math.max(0, Math.min(1000, this.pos[0] + dx));
+        this.pos[1] = Math.max(0, Math.min(1000, this.pos[1] + dy));
+        this.publish(this.id, null, 'pos-changed', this.pos);
     }
 }
 
 
-function initIsland(state) {
-    return new Island(state, () => {
-        new Text({}, {id: 'text1'});
-    });
+class RootView extends View {
+
+    attach(model) {
+        super.attach(model);
+        this.element = document.createElement("div");
+        this.element.className = "root";
+        this.resize();
+        document.body.appendChild(this.element);
+        window.onresize = () => this.resize();
+        model.children.forEach(child => this.attachChild(child.id));
+        this.subscribePart(this.modelId, null, 'child-added', null, "attachChild", true);
+    }
+
+    // non-inherited methods below
+
+    attachChild(childID) {
+        const model = this.island.modelsById[childID];
+        const view = new ShapeView(this.island);
+        view.attach(model);
+        this.element.appendChild(view.element);
+    }
+
+    resize() {
+        const size = Math.max(200, Math.min(window.innerWidth, window.innerHeight) - 20);
+        SCALE = size / 1100;
+        this.element.style.transform = `translate(${(window.innerWidth - size) / 2}px,${10}px) scale(${SCALE})`;
+        this.element.style.transformOrigin = "0 0";
+    }
+
 }
+
+
+class ShapeView extends View {
+
+    attach(model) {
+        super.attach(model);
+        this.element = document.createElement("div");
+        this.element.className = model.type;
+        this.element.style.backgroundColor = model.color;
+        this.element.onmousedown = () => {
+            let dx = 0;
+            let dy = 0;
+            let timeStamp = 0;
+            document.onmousemove = evt => {
+                dx += evt.movementX;
+                dy += evt.movementY;
+                if (evt.timeStamp - timeStamp > THROTTLE) {
+                    this.model.moveBy(dx / SCALE, dy / SCALE);
+                    dx = dy = 0;
+                    timeStamp = evt.timeStamp;
+                }
+            };
+            document.onmouseup = () => {
+                document.onmouseup = null;
+                document.onmousemove = null;
+            };
+        };
+        this.subscribePart(this.modelId, null, 'pos-changed', null, "move", true);
+        this.move(model.pos);
+    }
+
+    // non-inherited methods below
+
+    move(pos) {
+        this.element.style.left = pos[0];
+        this.element.style.top = pos[1];
+    }
+
+}
+
 
 async function go() {
     connectToReflector("wss://dev1.os.vision/reflector-v1");
@@ -128,17 +141,34 @@ async function go() {
     const controller = new Controller();
     const island = await controller.createIsland("2d", {
         moduleID: module.id,
-        creatorFn: initIsland,
+        creatorFn(state) {
+            return new Island(state, () => {
+                const root = new Root();
+                for (let i = 0; i < 100; i++) {
+                    root.add(new Shape());
+                }
+            });
+        }
     });
 
-    const textView = new TextView(island);
-    textView.attach(island.get('text'));
+    const rootView = new RootView(island);
+    rootView.attach(island.get('root'));
 
-    function run() {
+
+    window.requestAnimationFrame(frame);
+    function frame(timestamp) {
+        Stats.animationFrame(timestamp);
+        Stats.users(controller.users);
+        Stats.network(Date.now() - controller.lastReceived);
+
         controller.simulate(Date.now() + 200);
+
+        Stats.begin("render");
         island.processModelViewEvents();
+        Stats.end("render");
+
+        window.requestAnimationFrame(frame);
     }
-    setInterval(run, 50);
 }
 
 
---
+++ historic/simpleapp/src/2d/text.js
@@ -0,0 +1,142 @@
+import Model from "../model.js";
+import View from "../view.js";
+
+const COLORS = [ "#FFF", "#C00", "#0C0", "#880", "#00C", "#C0C", "#0CC", "#888" ];
+const CURSOR = "|";
+
+export class Text extends Model {
+
+    constructor(state) {
+        super(state);
+        this.island.set('text', this);
+        this.text = state.text || {
+            contents: '',
+            selections: {},
+        };
+    }
+
+    toState(state) {
+        super.toState(state);
+        state.text = this.text;
+    }
+
+    // non-inherited methods below
+
+    changed() {
+        this.publish(this.id, null, 'text-changed', this.text);
+    }
+
+    keydown(user, key, meta, shift) {
+        const sel = this.text.selections[user];
+        if (meta || key.length > 1) this.command(sel, key, meta, shift);
+        else {
+            this.replace(sel, key);
+        }
+    }
+
+    command(sel, key, meta, shift) {
+        if (key.length === 1) key = key.toUpperCase();
+        const l = this.text.contents.length;
+        switch (key) {
+            case "Enter":  this.replace(sel, '\n'); break;
+            case "ArrowRight": sel.end = Math.min(l, sel.end + 1);
+                if (!shift) sel.start = sel.end; break;
+            case "ArrowLeft": sel.start = Math.max(0, sel.start - 1);
+                if (!shift) sel.end = sel.start; break;
+            case "Backspace": if (sel.start === sel.end) sel.start = Math.max(0, sel.start - 1);
+                this.replace(sel, ''); break;
+            case 'Shift': case 'Control': case 'Alt': case 'Meta': return;
+                default:
+                console.warn(`Unhandled key: ${meta ? 'Meta-' : ''}${shift ? 'Shift-' : ''}${key}`);
+                return;
+        }
+        this.changed();
+    }
+
+    replace(sel, string) {
+        const text = this.text.contents;
+        this.text.contents = text.slice(0, sel.start) + string + text.slice(sel.end);
+        const delta = sel.end - sel.start + string.length;
+        for (const each of Object.values(this.text.selections)) {
+            if ((sel !== each) && each.start > sel.end) {
+                each.start += delta;
+                each.end += delta;
+            }
+        }
+        sel.end = sel.start + string.length;
+        sel.start = sel.end;
+        this.changed();
+    }
+
+    createSelection(user) {
+        if (!this.text.selections[user]) {
+            this.text.selections[user] = { start: 0, end: 0, color: this.unusedColor() };
+        }
+        this.changed();
+    }
+
+    unusedColor() {
+        const usedColors = Object.values(this.text.selections).map(sel => sel.color).sort();
+        let i = 0;
+        while (usedColors.includes(i)) i++;
+        return i;
+    }
+}
+
+
+export class TextView extends View {
+
+    attach(modelState) {
+        super.attach(modelState);
+        this.div = document.createElement("div");
+        this.render(modelState.text);
+        document.body.appendChild(this.div);
+
+        this.subscribePart(this.modelId, null, 'text-changed', null, "render", true);
+
+        document.addEventListener("keydown", evt => {
+            this.model.keydown(this.user, evt.key, evt.metaKey || evt.ctrlKey, evt.shiftKey);
+        });
+
+        this.user = localStorage.user || (localStorage.user = window.prompt("What's your name for this browser?"));
+        this.model.createSelection(this.user);
+    }
+
+    // non-inherited methods below
+
+    render(text) {
+        // clear old spans
+        while (this.div.firstChild) this.div.removeChild(this.div.firstChild);
+        // create characters
+        const chars = text.contents.split('').map(char => ({char, color: 0}));
+        // apply selections
+        const cursors = [];
+        for (const { start, end, color } of Object.values(text.selections)) {
+            if (start === end) cursors.push({start, color});
+            else for (let i = start; i < end; i++) {
+                chars[i].color |= 1 << color;
+            }
+        }
+        // insert cursors
+        for (const { start, color } of cursors.sort((a,b) => b.start - a.start)) {
+            const here = chars.length > 0 ? chars[Math.min(start, chars.length-1)].color : 0;
+            chars.splice(start, 0, { char: CURSOR, color: here | 1 << color });
+        }
+        chars.push({char: '', color: -1}); // force end of last span
+        // create spans
+        let prev = 0;
+        let string = '';
+        for (const {char, color} of Object.values(chars)) {
+            if (color !== prev && string) {
+                const span = document.createElement("span");
+                if (string === CURSOR) span.style.color = COLORS[prev % COLORS.length];
+                else span.style.backgroundColor = COLORS[prev % COLORS.length];
+                span.innerText = string;
+                this.div.appendChild(span);
+                string = '';
+            }
+            prev = color;
+            string += char;
+        }
+    }
+}
