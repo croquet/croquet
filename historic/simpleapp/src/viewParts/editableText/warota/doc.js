d4dcf9396d3ef7c9dfbc732859f34ac146df890e
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2019-04-04 20:15:42 -0700

    first cut of wordwrap

--- historic/simpleapp/src/viewParts/editableText/warota/doc.js
+++ historic/simpleapp/src/viewParts/editableText/warota/doc.js
@@ -1,7 +1,17 @@
+class Rect {
+    constructor(l, t, w, h) {
+        this.left = l;
+        this.top = t;
+        this.width = w;
+        this.height = h;
+    }
+}
+
 function length(ary) {
     return ary.map(c => c.text).reduce((s, x) => x.length + s, 0);
 }
 
+/*export*/
 class Insert {
     constructor(user, runs, pos, timezone) {
         this.user = user;
@@ -21,6 +31,7 @@ class Insert {
     type() {return "insert";}
 }
 
+/*export*/
 class Delete {
     constructor(user, start, end, timezone) {
         this.user = user;
@@ -43,27 +54,95 @@ class Delete {
     type() {return "delete";}
 }
 
-export default class Doc {
+/*export*/
+class Doc {
     constructor() {
         this._width = 0;
-        this.load([]);
+        this.doc = [{start: 0, end: 0, text: ""}]; // [{start: num, end: num, text: str, (opt)style: {font: str, size: num, color: str, emphasis: 'b' | 'i'|'bi'}}]
+
+        // created in layout, as it involves line wrapping.
+        // all letters in a "word" shares the same style,
+        // and never go across the displayed lines
+        // note that, a space character between letters is counted as a word
+        this.renderedWords = null; // [{start: num, end: num, text: string, left: num, top, num, width: num, height: num}]
+
+        this.defaultFont = "Roman";
+        this.defaultSize = 10;
+    }
+
+    setDefault(font, size) {
+        this.defaultFont = font;
+        this.defaultSize = size;
     }
 
     load(runs) {
+        // runs does not have start and end (a human would not want to add them).
+        // The canonicalize method adds them.  What save() would do is to strip them out.
         this.commands = [];
-        this.doc = this.canonicalize(runs); // [{start: num, end: num, text: str, style: <tbd>}]
+        this.doc = this.canonicalize(runs);
         this.layout();
     }
-  
+
     setMargins(margins) {
         this.margins = margins;
     }
 
+    equalStyle(prev, next, defaultFont, defaultSize) {
+        if (!prev && !next) {return true;}
+
+        if (!prev) {
+            return next.font === defaultFont && next.size === defaultSize
+               && !next.color && !next.emphasis;
+        }
+        if (!next) {
+            return prev.font === defaultFont && prev.size === defaultSize
+               && !prev.color && !prev.emphasis;
+        }
+
+        return (prev.font || defaultFont) === (next.font || defaultFont)
+            && (prev.size || defaultSize) === (next.size || defaultSize)
+            && (prev.color === next.color)
+            && (prev.emphasis === next.emphasis);
+    }
+
+    canonicalize(runs) {
+        let result = [];
+        let lastRun = runs[0];
+        let lastStyle = lastRun.style;
+        let start = 0;
+        let end = 0;
+        let i = 1;
+        let run = runs[i];
+        while (run) {
+            if (this.equalStyle(lastRun.style, run.style, this.defaultFont, this.defaultSize)) {
+                lastRun.text += run.text;
+            } else {
+                lastRun.start = start;
+                end = start + lastRun.text.length;
+                lastRun.end = end;
+                start = end;
+
+                result.push(lastRun);
+                lastRun = run;
+            }
+            i++;
+            run = runs[i];
+        }
+        lastRun.start = start;
+        end = start + lastRun.text.length;
+        lastRun.end = end;
+        result.push(lastRun);
+        return result;
+    }
+
+    splitLines(runs) {
+        // assumes that runs is canonicalized
+    }
+     
     layout() {
         this.frame = null;
 
         try {
-            this.frame = this.doLayout();
             let [lines, attrs] = this.splitLines(runs);
 
             // transient.  Constructed from this.doc
@@ -80,65 +159,72 @@ export default class Doc {
         //return;
     }
 
+    findLine(pos) {
+        // a smarty may do a binary search
+        return this.lines.find(line => line.start <= pos && pos < line.end);
+    }
+
+    findRun(pos) {
+        let ind = this.doc.findIndex(run => run.start <= run && pos < run.end);
+        if (ind < 0) {
+            ind = this.doc.length - 1;
+        }
+        return [this.doc[ind], ind];
+    }
+
+    findWord(pos, x, y) {
+        if (x !== undefined && y !== undefined) {
+            let wordIndex = this.words.findIndex(word => word.top + word.height >= y);
+            let word = this.words[wordIndex];
+            let top = word.top;
+            while (true) {
+                if (word.left <= x && x < word.left + word.width) {
+                    return [word, wordIndex];
+                }
+                if (word.isEOL) {
+                    // at the end of line
+                    return [word, wordIndex]
+                }
+                word = this.words[++wordIndex];
+            }
+            // last line?
+        }
+        let wordIndex = this.words.findIndex(word => word.start <= pos && pos < word.end);
+        return [word, wordIndex];
+    }
+
     insert(pos, runs) {
         // runs: [{text: <string>, style: <tbd>}]
 
-        let current = 0;
-        let runIndex = 0;
+        let [run, runIndex] = this.findRun(pos);
 
-        let nextRun;
-        let runLength;
-
-        do {
-            nextRun = this.doc[runIndex]; // (at least there should be "", in this.doc)
-            runLength = this.runLength(nextRun);
-            current += runLength;
-            runIndex += 1;
-
-        } while (current < pos && runIndex < this.doc.length);
-
-        if (current > pos) {
-            // the end of this run goes beyond pos
-            this.splitDocAt(runIndex, current - pos);
+        if (run.end !== pos) { // that is, pos is within the run
+            this.splitDocAt(runIndex, pos - run.start);
         }
-        this.doc.splice(runIndex, 0, runs); // adding it
-        this.doc = this.canonicalize(this.doc);
+        this.doc.splice(runIndex, 0, runs); // destructively adding the runs
+        this.doc = this.canonicalize(this.doc, run.start);
         this.layout();
     }
 
-    delete(start, end) {
-        let current = 0;
-        let runIndex = 0;
+    delete(pos, end) {
+        let [run, runIndex] = this.findRun(pos);
 
-        let nextRun;
-        let runLength;
-
-        do {
-            nextRun = this.doc[runIndex]; // (at least there should be "", in this.doc)
-            runLength = this.runLength(nextRun);
-            current += runLength;
-            runIndex += 1;
-
-        } while (current < start && runIndex < this.doc.length);
-
-        if (current > pos) {
-            // the end of this run goes beyond pos
-            this.splitDocAt(runIndex, current - start);
+        if (run.end !== pos) { // that is, pos is within the run
+            this.splitDocAt(runIndex, pos - run.start);
         }
 
-        let first = runIndex;
-
+        let endRun = run;
+        let endRunIndex;
         do {
-            nextRun = this.doc[runIndex]; // Is there a guarantee that this is in bounds?
-            runLength = this.runLength(nextRun);
-            current += runLength;
-            runIndex += 1;
+            [endRun, endRunIndex] = this.findRun(endRun.end);
+        } while (endRun.end < end && endRunIndex < this.doc.length);
 
-        } while (current < end && runIndex < this.doc.length);
+        let reminder = end - endRun.start;
+        if (end !== endRun.end) {
+            this.splitDocAt(endRunIndex, end - endRun.start);
+        }
 
-        let last = runIndex;
-
-        this.doc.splice(first, last);
+        this.doc.splice(runIndex, endRunIndex);
         this.doc = this.canonicalize(this.doc);
         this.layout();
     }
@@ -151,38 +237,25 @@ export default class Doc {
         this.layout();
     }
 
-    positionFromIndex(ind) {
-        // this.doc should be quiescent, and lines and attrs should be computed
-        let lineIndex = 0;
-        let current = 0;
-        let line;
+    positionFromIndex(pos) {
+        let [word, wordIndex] = this.findWord(pos);
 
-        do {
-            line = this.lines[lineIndex]; // (at least there should be "", in this.doc)
-            if (line.start <= pos && pos < line.end) {
-                break;
-            }
-            lineIndex += 1;
-        } while (lineIndex < this.lines.length);
-
-        do {
-            nextRun = this.doc[runIndex]; // Is there a guarantee that this is in bounds?
-            runLength = this.runLength(nextRun);
-            current += runLength;
-            runIndex += 1;
-
-        } while (current < end && runIndex < this.doc.length);
-
-        if (current > pos) {
-            // the end of this run goes beyond pos
-            this.splitDocAt(runIndex, current - start);
-        }
-
-        let first = runIndex;
+        let measure0 = this.measureText(word.text.slice(0, pos-1), word.style);
+        let measure1 = this.measureText(word.text.slice(0, pos), word.style);
+        return new Rect(word.left + measure0.width, word.top, measure1.width - measure0.width, word.height);
     }
 
-  indexFromPosition(x, y) {
-  }
+    indexFromPosition(x, y) {
+        let [word, wordIndex] = this.findWord(null, x, y);
+
+        for (let i = 0; i < word.text.length; i++) {
+            let measure = this.measureText(word.text.slice(0, i), word.style);
+            if (measure.width > y - word.left) {
+                return word.start + i;
+            }
+        }
+        return 0;
+    }
 
   performUndo() {
       let command = this.commands.pop();
---
+++ historic/simpleapp/src/viewParts/editableText/warota/wrap.js
@@ -0,0 +1,198 @@
+function mockMeasurer(str, style) {
+    return {width: str.length * 10, height: 20};
+}
+
+function equalStyle(prev, next, defaultFont, defaultSize) {
+    if (!prev && !next) {return true;}
+
+    if (!prev) {
+        return next.font === defaultFont && next.size === defaultSize
+            && !next.color && !next.emphasis;
+    }
+    if (!next) {
+        return prev.font === defaultFont && prev.size === defaultSize
+            && !prev.color && !prev.emphasis;
+    }
+
+    return (prev.font || defaultFont) === (next.font || defaultFont)
+        && (prev.size || defaultSize) === (next.size || defaultSize)
+        && (prev.color === next.color)
+        && (prev.emphasis === next.emphasis);
+}
+
+/*export */
+function splitWords(runs) {
+    // returns words and lines.
+
+    const isSpace = (str) => /[ \f\n\r\t\v\u00A0\u2028\u2029]/.test(str);
+    const isNewline = (str) => /[\n\r]/.test(str);
+
+    let words = [];
+    let lines = [];
+
+    let currentLine = [];
+    let currentHeight = 0;
+
+    let isInWord;
+    let start = 0;
+    let leftOver = "";
+    let styles = null;
+    let thisWord;
+
+    let push = (obj, style, ms) => {
+        if (ms && ms.length > 1) {
+            words.push(Object.assign(obj, {styles: ms}));
+        } else if (ms && ms.length === 1) {
+            if (ms[0].style) {
+                words.push(Object.assign(obj, {style: ms[0]}));
+            } else {
+                words.push(obj);
+            }
+        } else if (style) {
+            if (style.style) {
+                words.push(Object.assign(obj, {style}));
+            } else {
+                words.push(obj);
+            }
+        } else {
+            words.push(obj);
+        }
+    };
+
+    let stylePush = (ms, newOne) => {
+        if (!ms) {
+            return [newOne];
+        }
+        let last = styles[styles.length-1];
+        if (!equalStyle(last.style, newOne.style)) {
+            styles.push(newOne);
+            return styles;
+        }
+        last.end = newOne.end;
+        return styles;
+    }
+
+    let style;
+    for (let i = 0; i < runs.length; i++) {
+        let run = runs[i];
+        let text = run.text;
+        style = run.style;
+        if (start === 0  && i === 0) {
+            isInWord = !isSpace(text[start]);
+        }
+
+        let wordStart = 0;
+        let runStart = 0;
+        for (let j = 0; j < text.length; j++) {
+            if (start === 0 && i === 0) {continue;}
+            if (isInWord) {
+                if (isSpace(text[j])) {
+                    thisWord = text.slice(wordStart, j);
+                    if (leftOver.length > 0) {
+                        if (thisWord.length > 0) {
+                            let newOne = {start: leftOver.length, end: leftOver.length + thisWord.length, style: style};
+                            styles = stylePush(styles, newOne);
+                        }
+                        thisWord = leftOver + thisWord;
+                        leftOver = "";
+                    }
+                    push({start, end: start + thisWord.length, text: thisWord}, style, styles);
+                    start += start + thisWord.length;
+                    wordStart = j;
+                    isInWord = false;
+                    styles = null;
+                }
+            } else {
+                push({start, end: start + 1, text: text[j-1], style, styles});
+                styles = null;
+                start += 1;
+                wordStart += 1;
+                if (!isSpace(text[j])) {
+                    isInWord = true;
+                }
+            }
+        }
+        thisWord = text.slice(wordStart, text.length);
+        let fragment = {start: leftOver.length, end: leftOver.length + thisWord.length, style: style};
+        styles = stylePush(styles, fragment);
+        leftOver += thisWord;
+    }
+    push({start, end: start + leftOver.length, text: leftOver}, style, styles);
+    return words;
+}
+
+function mergeRect(m1, m2) {
+    if (!m1) {return m2;}
+    if (!m2) {return m1;}
+    return {width: m1.width + m2.width,
+            height: Math.min(m1.height, m2.height)};
+}
+
+/*export */
+function wrap(runs, textWidth, measurer, margins={left: 0, top: 0, right: 0, bottom: 0}) {
+    // returns words and lines.
+
+    const width = textWidth - margins.left - margins.right;
+    const isSpace = (str) => /[ \f\n\r\t\v\u00A0\u2028\u2029]/.test(str);
+    const isNewline = (str) => /[\n\r]/.test(str);
+
+    let currentLine = [];
+    let currentHeight = 0;
+    let lines = []; // list of list of words
+
+    let left = margins.left;
+    let top = margins.top;
+
+    let words = splitWords(runs);
+
+    for (let w = 0; w < words.length; w++) {
+        let word = words[w];
+        let rect;
+
+        if (isNewline(word.text)) {
+            rect = mockMeasurer(' ', word.style);
+            currentHeight = Math.max(currentHeight, rect.height);
+            rect.left = left;
+            word.rect = rect;
+            currentLine.push(word);
+            currentLine.forEach(c => {
+                c.rect.height = currentHeight;
+            });
+            lines.push(currentLine);
+            currentLine = [];
+            left = margins.left;
+            top += currentHeight;
+            continue;
+        }
+
+        if (word.styles) {
+            // a word with multiple styles
+            for (let i = 0; i < word.styles.length; i++) {
+                let m = mockMeasurer(word.text.slice(word.styles[i].start, word.styles[i].end), word.styles[i]);
+                rect = mergeRect(rect, m);
+            }
+        } else {
+            rect = mockMeasurer(word.text, word.style);
+        }
+        currentHeight = Math.max(currentHeight, rect.height);
+
+        if (rect.width + left > width) {
+            currentLine.forEach(c => {
+                c.height = currentHeight;
+            });
+            lines.push(currentLine);
+            currentLine = [];
+            left = margins.left;
+            top += currentHeight;
+        }
+        rect.left = left;
+        word.rect = rect;
+        left += rect.width;
+        currentLine.push(word);
+    }
+
+    if (currentLine.length > 0) {
+        lines.push(currentLine);
+    }
+    return lines;
+}
