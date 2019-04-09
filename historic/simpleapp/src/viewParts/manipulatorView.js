38e1a144a3fe21603aa8ef86f7348a4a5f9f45ae
Anselm Eickhoff <anselm.eickhoff@gmail.com>
2019-04-09 15:47:56 -0700

    Preparation for view-facing object manipulators

--- historic/simpleapp/src/viewParts/manipulatorView.js
+++ historic/simpleapp/src/viewParts/manipulatorView.js
@@ -9,7 +9,7 @@ import Tracking from "./tracking.js";
 const moduleVersion = module.bundle.v ? (module.bundle.v[module.id] || 0) + 1 : 0;
 if (module.bundle.v) { console.log(`Hot reload ${module.id}#${moduleVersion}`); module.bundle.v[module.id] = moduleVersion; }
 
-class ManipulatorViewPart extends ViewPart {
+class TranslationManipulator extends ViewPart {
     constructor(model, options) {
         options = {target: "spatial", ...options};
         super(model, options);
@@ -23,21 +23,13 @@ class ManipulatorViewPart extends ViewPart {
             0.5,
             false
         );
-        this.rotateHandle = new SVGIcon(
-            rotateHandle,
-            new THREE.MeshBasicMaterial({color: "#ffffff"}),
-            new THREE.MeshBasicMaterial({color: "#ffffff", polygonOffset: true, polygonOffsetFactor: 0.1, transparent: true, opacity: 0.2}),
-            1.5
-        );
+
         this.moveHandle.position.y -= 0.8;
         this.moveHandle.position.z = 0.2;
         makePointerSensitive(this.moveHandle, this);
-        this.rotateHandle.position.y -= 0.7;
-        makePointerSensitive(this.rotateHandle, this);
         this.group.add(this.moveHandle);
-        this.group.add(this.rotateHandle);
 
-        this.subscribe(PointerEvents.pointerMove, "onPointerMove");
+        this.subscribe(PointerEvents.pointerEnter, "onPointerEnter");
         this.subscribe(PointerEvents.pointerLeave, "onPointerLeave");
         this.subscribe(PointerEvents.pointerDown, "onPointerDown");
         this.subscribe(PointerEvents.pointerDrag, "onPointerDrag");
@@ -45,23 +37,60 @@ class ManipulatorViewPart extends ViewPart {
         this.threeObj = this.group;
     }
 
-    onPointerMove({hoverThreeObj}) {
-        if (hoverThreeObj === this.moveHandle) {
-            this.moveHandle.altMaterial.color = new THREE.Color("#A1DCD4");
-            this.moveHandle.altMaterial.opacity = 1;
-            this.rotateHandle.altMaterial.color = new THREE.Color("#ffffff");
-            this.rotateHandle.altMaterial.opacity = 0.2;
-        } else {
-            this.rotateHandle.altMaterial.color = new THREE.Color("#A1DCD4");
-            this.rotateHandle.altMaterial.opacity = 1;
-            this.moveHandle.altMaterial.color = new THREE.Color("#ffffff");
-            this.moveHandle.altMaterial.opacity = 0.2;
-        }
+    onPointerEnter() {
+        this.moveHandle.altMaterial.color = new THREE.Color("#A1DCD4");
+        this.moveHandle.altMaterial.opacity = 1;
     }
 
     onPointerLeave() {
         this.moveHandle.altMaterial.color = new THREE.Color("#ffffff");
         this.moveHandle.altMaterial.opacity = 0.2;
+    }
+
+    onPointerDown() {
+        this.positionAtDragStart = this.group.position.clone();
+        this.quaternionAtDragStart = this.group.quaternion.clone();
+    }
+
+    onPointerDrag({dragStart, dragEndOnHorizontalPlane}) {
+        this.modelPart(this.target).moveTo(
+            this.positionAtDragStart.clone().add(dragEndOnHorizontalPlane.clone().sub(dragStart))
+        );
+    }
+}
+
+class RotationManipulator extends ViewPart {
+    constructor(model, options) {
+        options = {target: "spatial", ...options};
+        super(model, options);
+
+        this.target = options.target;
+        this.group = new THREE.Group();
+        this.rotateHandle = new SVGIcon(
+            rotateHandle,
+            new THREE.MeshBasicMaterial({color: "#ffffff"}),
+            new THREE.MeshBasicMaterial({color: "#ffffff", polygonOffset: true, polygonOffsetFactor: 0.1, transparent: true, opacity: 0.2}),
+            1.5
+        );
+
+        this.rotateHandle.position.y -= 0.7;
+        makePointerSensitive(this.rotateHandle, this);
+        this.group.add(this.rotateHandle);
+
+        this.subscribe(PointerEvents.pointerEnter, "onPointerEnter");
+        this.subscribe(PointerEvents.pointerLeave, "onPointerLeave");
+        this.subscribe(PointerEvents.pointerDown, "onPointerDown");
+        this.subscribe(PointerEvents.pointerDrag, "onPointerDrag");
+
+        this.threeObj = this.group;
+    }
+
+    onPointerEnter() {
+        this.rotateHandle.altMaterial.color = new THREE.Color("#A1DCD4");
+        this.rotateHandle.altMaterial.opacity = 1;
+    }
+
+    onPointerLeave() {
         this.rotateHandle.altMaterial.color = new THREE.Color("#ffffff");
         this.rotateHandle.altMaterial.opacity = 0.2;
     }
@@ -71,20 +100,14 @@ class ManipulatorViewPart extends ViewPart {
         this.quaternionAtDragStart = this.group.quaternion.clone();
     }
 
-    onPointerDrag({dragStart, dragEndOnHorizontalPlane, dragStartThreeObj}) {
-        if (dragStartThreeObj === this.moveHandle) {
-            this.modelPart(this.target).moveTo(
-                this.positionAtDragStart.clone().add(dragEndOnHorizontalPlane.clone().sub(dragStart))
-            );
-        } else if (dragStartThreeObj === this.rotateHandle) {
-            const delta = (new THREE.Quaternion()).setFromUnitVectors(
-                dragStart.clone().sub(this.positionAtDragStart).setY(0).normalize(),
-                dragEndOnHorizontalPlane.clone().sub(this.positionAtDragStart).setY(0).normalize(),
-            );
-            this.modelPart(this.target).rotateTo(
-                this.quaternionAtDragStart.clone().multiply(delta)
-            );
-        }
+    onPointerDrag({dragStart, dragEndOnHorizontalPlane}) {
+        const delta = (new THREE.Quaternion()).setFromUnitVectors(
+            dragStart.clone().sub(this.positionAtDragStart).setY(0).normalize(),
+            dragEndOnHorizontalPlane.clone().sub(this.positionAtDragStart).setY(0).normalize(),
+        );
+        this.modelPart(this.target).rotateTo(
+            this.quaternionAtDragStart.clone().multiply(delta)
+        );
     }
 }
 
@@ -94,7 +117,8 @@ export default function WithManipulator(BaseViewPart) {
             super(model, {});
             this.parts = {
                 inner: new BaseViewPart(model, options),
-                manipulator: new (Tracking(ManipulatorViewPart, {scale: false}))(model, {})
+                translationManipulator: new (Tracking(TranslationManipulator, {scale: false, rotation: false}))(model, {}),
+                RotationManipulator: new (Tracking(RotationManipulator, {scale: false}))(model, {}),
             };
         }
     };
