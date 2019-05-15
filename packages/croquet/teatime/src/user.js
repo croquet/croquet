a51c97c128d300d047300ce963dc554882481f40
Vanessa Freudenberg <vanessa@codefrau.net>
2019-05-14 19:12:33 -0700

    teatime: break out style

--- packages/croquet/teatime/src/user.js
+++ packages/croquet/teatime/src/user.js
@@ -25,56 +25,62 @@ export function getUser(key, defaultValue=undefined, initFn=null) {
 
 // login user
 if (!getUser("name") && DEBUG.user) {
-    const dialog = document.createElement("div");
-    dialog.setAttribute("style", "background-color:#fff;color:#666;border-radius:10px;padding:16px;font-size:14px;");
+    const style = document.createElement("style");
+    style.innerHTML = `
+        form {
+            background-color: #fff;
+            color: #666;
+            border-radius: 10px;
+            padding: 16px;
+            font-size: 14px;
+        }
+        dt { margin: 4px 0 }
+        dd { margin-left: 0 }
+        input {
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            min-height: 46px;
+            padding: 10px;
+            font-size: 16px;
+            width: 100%;
+            box-shadow: inset 0 1px 2px #666;
+        }
+        input:invalid { border: 1px solid red; }
+        input:valid   { border: 1px solid #ccc; }
+        button {
+            padding: 20px 32px;
+            background-color: #3b5;
+            color: #fff;
+            border-radius: 50px;
+            border-width:0;
+            font-size: 16px;
+            font-weight: 500;
+            width: 100%;
+        }
+    `;
+    document.head.appendChild(style);
+
+    const dialog = document.createElement("form");
     dialog.innerHTML = `
         <dl>
-            <dt style="margin: 4px 0"><label for="user[name]">Username</label></dt>
-            <dd style="margin-left: 0">
-                <input type="text" id="user[name]" placeholder="Pick a username" autocomplete="off" spellcheck="false"
-                style="border-radius: 5px;
-                    min-height: 46px;
-                    padding: 10px;
-                    font-size: 16px;
-                    width: 100%;
-                    border: 1px solid #ccc;
-                    box-shadow: inset 0 1px 2px #666;
-                "
-                ></input>
+            <dt><label for="user[name]">Username</label></dt>
+            <dd>
+                <input type="text" pattern="^[a-zA-Z0-9_]*$" name="user[name]" id="user[name]" placeholder="Pick a username" autocomplete="off" spellcheck="false"></input>
             </dd>
         </dl>
         <dl>
-            <dt style="margin: 4px 0"><label for="user[email]">Email</label></dt>
-            <dd style="margin-left: 0">
-                <input type="text" id="user[email]" placeholder="you@example.com" autocomplete="off" spellcheck="false"
-                style="border-radius: 5px;
-                    min-height: 46px;
-                    padding: 10px;
-                    font-size: 16px;
-                    width: 100%;
-                    border: 1px solid #ccc;
-                    box-shadow: inset 0 1px 2px #666;
-                "
-                ></input>
+            <dt><label for="user[email]">Email</label></dt>
+            <dd>
+                <input type="email" id="user[email]" placeholder="you@example.com" autocomplete="off" spellcheck="false"></input>
             </dd>
         </dl>
         <dl>
-            <dt style="margin: 4px 0"><label for="user[password]">Password</label></dt>
-            <dd style="margin-left: 0">
-                <input type="password" id="user[password]" placeholder="Create a password" autocomplete="off" spellcheck="false"
-                style="border-radius: 5px;
-                    min-height: 46px;
-                    padding: 10px;
-                    font-size: 16px;
-                    width: 100%;
-                    border: 1px solid #ccc;
-                    box-shadow: inset 0 1px 2px #666;
-                "
-                ></input>
+            <dt><label for="user[password]">Password</label></dt>
+            <dd>
+                <input type="password" id="user[password]" placeholder="Create a password" autocomplete="off" spellcheck="false"></input>
             </dd>
         </dl>
-        <button style="padding: 20px 32px; background-color: #3b5; color: #fff; border-radius: 50px; border-width:0; font-size: 16px; font-weight: 500; width=100%;"
-        >Create Croquet Credentials</button>
+        <button id="user[create]">Create Croquet Credentials</button>
         <div style="max-width:280px;text-align: center;font-size:11px;margin:4px auto">
             By clicking “Create Croquet Credentials”,<br>
             you agree to our
@@ -83,8 +89,7 @@ if (!getUser("name") && DEBUG.user) {
             <a href="/privacy" target="_blank" style="color:#36c;text-decoration:none">privacy statement</a>.
         </div>
         <div style="margin:16px auto;text-align: center">— OR —</div>
-        <button style="padding: 20px 32px; background-color: #999; color: #fff; border-radius: 50px; border-width:0; font-size: 16px; font-weight: 500; width=100%;"
-        >Continue as Croquet Guest</button>
+        <button id="user[guest]" style="background-color: #999">Continue as Croquet Guest</button>
         <div style="max-width:250px;text-align: center;font-size:11px;margin:4px auto">Your stuff will disappear eventually.</div>
     `;
     const overlay = document.createElement("div");
