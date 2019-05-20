b40814e56ca4813d34b30a860171340d9b719533
Vanessa Freudenberg <vanessa@codefrau.net>
2019-05-20 14:23:30 -0700

    util: add qrcode lib

--- historic/util/html.js
+++ historic/util/html.js
@@ -1,4 +1,5 @@
 import SeedRandom from "seedrandom";
+import QRCode from "./thirdparty-patched/qrcodejs/qrcode";
 
 export function displaySessionMoniker(id='', element='session') {
     const button = document.getElementById(element);
@@ -17,4 +18,22 @@ export function displaySessionMoniker(id='', element='session') {
         const hash = [0,0,0,0].map(_=>(random.int32()>>>0).toString(16).padStart(8, '0')).join('');
         button.style.backgroundImage = `url('https://www.gravatar.com/avatar/${hash}?d=identicon&f=y')`;
     }
+}
+
+let qrcode;
+
+export function displayQRCode(url, div='qrcode') {
+    if (typeof div === "string") div = document.getElementById(div);
+    if (!div) return;
+    if (!url) url = window.location.href;
+    if (!qrcode) qrcode = new QRCode(div, {
+        text: url,
+        width: 128,
+        height: 128,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.L,   // L, M, Q, H
+    });
+    else qrcode.makeCode(url);
+    div.href = url; // if div is an <a> tag, make it work
 }
