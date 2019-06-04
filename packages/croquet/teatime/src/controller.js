acdebfac44d74c620b9e5698bdb108d719d81014
Vanessa Freudenberg <vanessa@codefrau.net>
2019-06-04 14:06:56 -0700

    teatime: publish "synced" event (true/false) when backlog is below / above threshold

--- packages/croquet/teatime/src/controller.js
+++ packages/croquet/teatime/src/controller.js
@@ -752,6 +752,7 @@ export default class Controller {
             if (this.synced && backlog > SYNCED_MAX || !this.synced && backlog < SYNCED_MIN) {
                 this.synced = !this.synced;
                 displaySpinner(!this.synced);
+                this.island.publishFromView(this.id, "synced", this.synced);
             }
             if (weHaveTime && this.cpuTime > SNAPSHOT_EVERY) { this.cpuTime = 0; this.scheduleSnapshot(); }
             return weHaveTime;
