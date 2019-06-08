b680317532fd158bf15dc9f492694a580dd9e78b
Vanessa Freudenberg <vanessa@codefrau.net>
2019-06-08 14:25:44 -0700

    teatime: add sim load balancing into simple api

--- packages/croquet/teatime/index.js
+++ packages/croquet/teatime/index.js
@@ -43,15 +43,30 @@ export async function startSession(name, ModelRoot=Model, ViewRoot=View, options
     }
 }
 
+/** maximum amount of time in milliseconds the model get to spend running its simulation */
+const MAX_SIMULATION_MS = 200;
+
+/** time spent simulating the last few frames */
+const simLoad = [];
+/** number of frames to spread load */
+const loadBalance = 4;
+/** time in ms we allow sim to lag behind before increasing sim budget */
+const balanceMS = loadBalance * (1000 / 60);
+
 function stepSession(frameTime, controller) {
     const {backlog, latency, starvation, activity} = controller;
     Stats.animationFrame(frameTime, {backlog, starvation, latency, activity, users: controller.users});
 
     if (controller.island) {
-        controller.simulate(Date.now() + 200);
+        const simStart = Date.now();
+        const simBudget = simLoad.reduce((a,b) => a + b, 0) / simLoad.length;
+        controller.simulate(simStart + Math.min(simBudget, MAX_SIMULATION_MS));
+        if (controller.backlog > balanceMS) controller.simulate(simStart + MAX_SIMULATION_MS - simBudget);
+        simLoad.push(Date.now() - simStart);
+        if (simLoad.length > loadBalance) simLoad.shift();
 
-        Stats.begin("render");
+        Stats.begin("update");
         controller.processModelViewEvents();
-        Stats.end("render");
+        Stats.end("update");
     }
 }
