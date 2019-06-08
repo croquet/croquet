d793edee963309b483578c3893bcb2e2a605d64c
Vanessa Freudenberg <vanessa@codefrau.net>
2019-06-07 19:51:48 -0700

    teatime: provide simple startSession() API

--- packages/croquet/teatime/index.js
+++ packages/croquet/teatime/index.js
@@ -1,7 +1,42 @@
+import { displaySessionMoniker, displayQRCode } from "@croquet/util/html";
+
+import Model from "./src/model";
+import View from "./src/view";
+import Controller from "./src/controller";
+
+export { Model, View, Controller };
+export { currentRealm } from "./src/realms";
+
 const moduleVersion = module.bundle.v ? (module.bundle.v[module.id] || 0) + 1 : 0;
 if (module.bundle.v) { console.log(`Hot reload ${module.id}#${moduleVersion}`); module.bundle.v[module.id] = moduleVersion; }
 
-export { default as Model } from "./src/model";
-export { default as View } from "./src/view";
-export { default as Controller } from "./src/controller";
-export { currentRealm } from "./src/realms";
+
+export async function startSession(name, ModelRoot=Model, ViewRoot=View, options={}) {
+
+    const controller = new Controller();
+    await bootModelView();
+    return controller;
+
+    async function bootModelView(snapshot) {
+        clear();
+        const modelRoot = (await controller.establishSession(name, {snapshot, init: spawnModel, destroyerFn: bootModelView, ...options})).model;
+        displaySessionMoniker(controller.id);
+        displayQRCode();
+        controller.inViewRealm(() => {
+            controller.view = new ViewRoot(modelRoot);
+        });
+    }
+
+    function clear() {
+        if (controller.view) {
+            controller.view.destroy();
+            controller.view = null;
+        }
+        displaySessionMoniker('');
+    }
+
+    function spawnModel() {
+        const modelRoot = ModelRoot.create();
+        return { model: modelRoot };
+    }
+}
