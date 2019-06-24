91ebd5b524d4f3598253e931cfd11577603b6699
Anselm Eickhoff <anselm.eickhoff@gmail.com>
2019-06-24 17:48:20 +0300

    integrations: croquet-react & demos

---
+++ historic/integrations/demos/croquet-react-hello-world/index.jsx
@@ -0,0 +1,43 @@
+import ReactDom from 'react-dom';
+import React from 'react';
+import {ObservableModel} from 'croquet';
+import {usePublish, useModelRoot, useObservable, InCroquetSession} from 'croquet-react';
+
+class HelloWorldModel extends ObservableModel({count: 0}) {
+    init() {
+        super.init();
+        this.future(1000).tick();
+        this.subscribe("counter", "reset", () => this.resetCounter());
+    }
+
+    resetCounter() {
+        this.count = 0;
+    }
+
+    tick() {
+        this.count += 1;
+        this.future(1000).tick();
+    }
+}
+
+HelloWorldModel.register();
+
+function HelloWorldApp() {
+    return <InCroquetSession name="pong" modelRoot={HelloWorldModel}>
+        <Counter/>
+    </InCroquetSession>;
+}
+
+function Counter() {
+    /** @type {PongModel} */
+    const model = useModelRoot();
+
+    const {count} = useObservable(model);
+    const publishReset = usePublish(publish => publish("counter", "reset"), []);
+
+    return <div onClick={publishReset} style={{margin: "1em", fontSize: "3em", cursor: "pointer"}}>
+        {count}
+    </div>;
+}
+
+ReactDom.render(<HelloWorldApp/>, document.getElementById("app"));
