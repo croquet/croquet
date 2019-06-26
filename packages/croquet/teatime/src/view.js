3571d8d2f691c0bf27dacdea5277cecb9e94c278
Vanessa Freudenberg <vanessa@codefrau.net>
2019-06-26 13:02:38 -0700

    teatime: make new subscribe work in view

--- packages/croquet/teatime/src/view.js
+++ packages/croquet/teatime/src/view.js
@@ -114,6 +114,7 @@ class View {
      * @public
      */
     subscribe(scope, eventSpec, callback) {
+        if (this[callback.name] === callback) callback = callback.bind(this);
         const {event, handling} = eventSpec.event ? eventSpec : {event: eventSpec};
         this.realm.subscribe(event, this.id, callback, scope, handling);
     }
