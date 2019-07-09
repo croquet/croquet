859c1fd23ba5dfe6197dcf2272889cd6483c8741
Vanessa Freudenberg <vanessa@codefrau.net>
2019-07-08 17:35:51 -0700

    [parcel] less intrusive parcel detection

--- packages/croquet/teatime/src/model.js
+++ packages/croquet/teatime/src/model.js
@@ -540,13 +540,12 @@ let ModelClasses = {};
 const CLASS_ID = Symbol('CLASS_ID');
 
 function gatherModelClasses() {
-    if (module.bundle) {
-        // HACK: go through all exports and find model subclasses
-        for (const [file, m] of Object.entries(module.bundle.cache)) {
-            for (const [name, cls] of Object.entries(m.exports)) {
-                if (cls && cls.__isTeatimeModelClass__) {
-                    registerClass(file, name === "default" ? cls.name : name, cls);
-                }
+    if (!module.bundle) return;
+    // HACK: go through all exports and find model subclasses
+    for (const [file, m] of Object.entries(module.bundle.cache)) {
+        for (const [name, cls] of Object.entries(m.exports)) {
+            if (cls && cls.__isTeatimeModelClass__) {
+                registerClass(file, name === "default" ? cls.name : name, cls);
             }
         }
     }
