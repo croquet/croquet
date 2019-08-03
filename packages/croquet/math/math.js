55f2be78c6f739d4b08da6b508a1f542e80b3737
Aran Lunzer <aran@acm.org>
2019-08-02 18:00:58 -0700

    [teatime] patch Math.pow

--- packages/croquet/math/math.js
+++ packages/croquet/math/math.js
@@ -26,11 +26,34 @@ window.CroquetMath.log = require("@stdlib/math/base/special/ln");      // ln bec
 window.CroquetMath.log1p = require("@stdlib/math/base/special/log1p");
 window.CroquetMath.log10 = require("@stdlib/math/base/special/log10");
 window.CroquetMath.log2 = require("@stdlib/math/base/special/log2");
-window.CroquetMath.pow = require("@stdlib/math/base/special/pow");
 window.CroquetMath.sin = require("@stdlib/math/base/special/sin");
 window.CroquetMath.sinh = require("@stdlib/math/base/special/sinh");
 window.CroquetMath.tan = require("@stdlib/math/base/special/tan");
 window.CroquetMath.tanh = require("@stdlib/math/base/special/tanh");
+
+// workaround for iOS Safari bug giving inconsistent results for stdlib's pow()
+//window.CroquetMath.pow = require("@stdlib/math/base/special/pow");
+const mathPow = Math.pow;
+function isInfinite(x) { return x === Infinity || x === -Infinity; }
+function isInteger(x) { return Number.isInteger(x); }
+window.CroquetMath.pow = (x, y) => {
+    if (isNaN(x) || isNaN(y)) return NaN;
+    if (isInfinite(x) || isInfinite(y)) return mathPow(x, y);
+    if (x === 0 || y === 0) return mathPow(x, y);
+    if (x < 0 && !isInteger(y)) return NaN;
+    if (isInteger(x) && isInteger(y)) return mathPow(x, y); // special-case integers
+
+    // remaining cases:
+    // x -ve non-integer, y integer
+    // x integer (+ve or -ve), y non-integer
+    let signResult = 1;
+    if (x < 0) {
+        x *= -1;
+        signResult = mathPow(-1, y);
+    }
+    const absPow = window.CroquetMath.exp(window.CroquetMath.log(x) * y);
+    return absPow * signResult;
+    };
 
 // if someone can figure out how to make this work properly with exports
 // then please change the import in island.js to the proper
