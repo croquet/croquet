13d8ac9ac60a49b3bfd3c4900f70023fb2243e2b
Vanessa Freudenberg <vanessa@codefrau.net>
2019-08-20 12:16:40 -0700

    [reflector] log location

--- packages/reflector/reflector.js
+++ packages/reflector/reflector.js
@@ -584,7 +584,7 @@ server.on('connection', (client, req) => {
         client.send(data);
         STATS.OUT += data.length;
     };
-    LOG(`connection #${server.clients.size} from ${client.addr}`);
+    LOG(`connection #${server.clients.size} from ${client.addr} ${client.location}`);
     STATS.USERS = Math.max(STATS.USERS, server.clients.size);
 
     let lastActivity = Date.now();
