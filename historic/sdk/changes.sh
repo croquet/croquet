9bddd167e4910c4c0ac52f16f14fd57231590a7c
Vanessa Freudenberg <vanessa@codefrau.net>
2019-10-01 14:49:47 -0700

    [sdk] fix changes script

--- historic/sdk/changes.sh
+++ historic/sdk/changes.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Show changes since last tagged release (prereleases are not tagged)
-#git log $(git describe --tags --abbrev=0)..HEAD -p -- ../teatime ../util ../reflector "@"
-git diff $(git describe --tags --abbrev=0) -- ../teatime ../util ../reflector "$@"
+git diff $(git tag -l 'v*' --sort creatordate|tail -1) -- ../teatime ../math ../util ../reflector "$@"
