571d50de7f352ff7defd4fc2bfbc18cb3b77d9a6
Vanessa Freudenberg <vanessa@codefrau.net>
2019-10-02 17:18:19 -0700

    [sdk] tweak changes.sh

--- historic/sdk/changes.sh
+++ historic/sdk/changes.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Show changes since last tagged release (prereleases are not tagged)
-git diff $(git tag -l 'v*' --sort creatordate|tail -1) -- ../teatime ../math ../util ../reflector "$@"
+RELEASE=$(git tag -l 'v*' --sort creatordate | tail -1)
+git diff $RELEASE -- ../teatime ../math ../util ../reflector "$@"
