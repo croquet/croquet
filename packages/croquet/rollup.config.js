f3c2a61861bd4a9f85a59624191171e592b2d736
Anselm Eickhoff <anselm.eickhoff@gmail.com>
2019-11-14 18:11:17 +0100

    [npm] Interpolate .env croquet version and experiment with watching

--- packages/croquet/rollup.config.js
+++ packages/croquet/rollup.config.js
@@ -2,6 +2,10 @@ import resolve from 'rollup-plugin-node-resolve';
 import license from 'rollup-plugin-license';
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
+import replace from 'rollup-plugin-replace';
+require('dotenv-flow').config({
+    default_node_env: 'development'
+});
 
 export default {
     input: 'src.js',
@@ -12,6 +16,11 @@ export default {
     },
     external: ['seedrandom/seedrandom', 'toastify-js', 'seedrandom', 'fast-json-stable-stringify', 'fastpriorityqueue'],
     plugins: [
+        // TODO: we might need something more elaborate once simple string replacement doesn't cut it anymore,
+        // see https://github.com/rollup/rollup/issues/487#issuecomment-486229172
+        replace({
+            'process.env.CROQUET_VERSION': JSON.stringify(process.env.CROQUET_VERSION)
+        }),
         resolve({only: [/^@croquet/]}),
         babel({
             externalHelpers: false, runtimeHelpers: true,
@@ -25,7 +34,7 @@ export default {
             banner: `Copyright Croquet Studio <%= moment().format('YYYY') %>
 Bundle of <%= pkg.name %>
 Generated: <%= moment().format('YYYY-MM-DD') %>
-Version: <%= pkg.version %>`,
+Version: <%= process.env.CROQUET_VERSION %>`,
         })
     ]
 };
