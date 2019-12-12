dc743b8784172340035f10a6d90e73d9d18bbfcb
Anselm Eickhoff <anselm.eickhoff@gmail.com>
2019-12-12 10:42:05 +0100

    Bump versions and fix croquet 3d types

--- packages/croquet-3d-physics/quaternion-to-euler.d.ts
+++ packages/croquet-3d-physics/quaternion-to-euler.d.ts
@@ -1,5 +1,5 @@
 
 declare module 'quaternion-to-euler' {
     import { vec3, quat } from 'gl-matrix';
-    export default function quatToEuler(quat: [number, number, number, number] | quat): [number, number, number] | vec3;
+    export default function quatToEuler(quat: [number, number, number, number] | quat): vec3;
 }
