bad2ef6c233e22618a4cc85641ee6c59e7a6d93e
Anselm Eickhoff <anselm.eickhoff@gmail.com>
2020-02-28 21:22:17 +0100

    [docs] Improvements to docs, docgen and types.

--- packages/croquet-3d-r3f-basic-shapes/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {vec3, quat} from 'gl-matrix';
-
-export function Box({position, quaternion, width, height, depth}: {position: vec3, quaternion: quat, width: number, height: number, depth: number}) {
-    <mesh position={position} quaternion={quaternion}>
-        <boxGeometry attach="geometry" width={width} height={height} depth={depth}></boxGeometry>
-    </mesh>
-}
--- packages/croquet-3d-r3f-default-scene/react.d.ts
+++
@@ -1,6 +0,0 @@
-declare module JSX {
-    interface IntrinsicElements {
-        "gridHelper": any,
-        "hemisphereLight": any
-    }
-}
