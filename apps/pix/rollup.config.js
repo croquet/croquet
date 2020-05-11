dd9547764374a146f9433ece9b9bb89d0aa4920e
Vanessa Freudenberg <vanessa@codefrau.net>
2020-05-11 15:51:02 -0700

    [pix] preserve sourcemaps

--- apps/pix/rollup.config.js
+++ apps/pix/rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs'
 import license from 'rollup-plugin-license';
+import sourcemaps from 'rollup-plugin-sourcemaps';
 import { terser } from 'rollup-plugin-terser';
 
 const is_dev_build = process.env.BUILD !== 'production';
@@ -18,6 +19,7 @@ export default {
     plugins: [
         resolve(), // tells Rollup how to resolve stuff in node_modules
         commonjs(), // converts all modules (including @croquet/croquet) to ES modules
+        sourcemaps(), // use sourcemaps from source files
         !is_dev_build && terser({
             mangle: {module: true},
         }),
