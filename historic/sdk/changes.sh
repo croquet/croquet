7e2018e54c17b22fb20289987514b9db62283a91
Vanessa Freudenberg <vanessa@codefrau.net>
2020-06-08 17:03:18 -0700

    [sdk] update changes.sh

--- historic/sdk/changes.sh
+++ historic/sdk/changes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Show changes since last tagged release (prereleases are not tagged)
-RELEASE=$(git tag -l 'v*' --sort creatordate | tail -1)
+RELEASE=$(git tag -l '@croquet/croquet@*' --sort creatordate | tail -1)
 git diff $RELEASE -- ../teatime ../math ../util ../reflector "$@"
