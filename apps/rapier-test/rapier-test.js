23ab34c3322e45ac91194a689a76c909640914e1
Vanessa Freudenberg <vanessa@codefrau.net>
2020-08-07 15:14:54 -0700

    [rapier] serialize bodies and colliders

--- apps/rapier-test/rapier-test.js
+++ apps/rapier-test/rapier-test.js
@@ -9,8 +9,33 @@ class RapierModel extends Model {
             "RAPIER.World": {
                 cls: RAPIER.World,
                 write: world => world.takeSnapshot(),
-                read: snapshot => RAPIER.World.restoreSnapshot(snapshot),
+                read: snapshot => {
+                    const world = RAPIER.World.restoreSnapshot(snapshot);
+                    // provide a mapping solely for use in reading objects
+                    world._bodyMap = new Map();
+                    world._colliderMap = new Map();
+                    // world._jointMap = new Map();
+                    world.forEachRigidBody(body => { body.world = world; world._bodyMap.set(body.handle(), body); });
+                    world.forEachCollider(collider => { collider.world = world; world._colliderMap.set(collider.handle(), collider); });
+                    // world.forEachJoint(joint => { joint.world = world; world._jointMap.set(joint.handle(), joint); });
+                    return world;
+                },
             },
+            "RAPIER.RigidBody": {
+                cls: RAPIER.RigidBody,
+                write: body => ({world: body.world, handle: body.handle()}),
+                read: ({world, handle}) => world._bodyMap.get(handle),
+            },
+            "RAPIER.Collider": {
+                cls: RAPIER.Collider,
+                write: collider => ({world: collider.world, handle: collider.handle()}),
+                read: ({world, handle}) => world._colliderMap.get(handle),
+            },
+            // "RAPIER.Joint": {
+            //     cls: RAPIER.Joint,
+            //     write: joint => ({world: joint.world, handle: joint.handle()}),
+            //     read: ({world, handle}) => { debugger; return world._jointMap.get(handle)},
+            // },
         }
     }
 /*
@@ -18,21 +43,22 @@ class RapierModel extends Model {
 ISSUES:
 
 * separate objects refer to world, cannot be de-serialized separately (need ref to world)
+  => now hacking world ref into every engine object
+* there appears to be no world.forEachJoint iterator, nor join.handle() method, so we can't restore joints
 * Q: do we need to manual free() objects we retrieve from rapier?
+* Q: are handles unique to each world? Can we have multiple rapier worlds at the same time?
 
 */
     init() {
         this.world = new RAPIER.World(0.0, -9.81, 0.0);
-        this.world.maxVelocityIterations = 4;
-        this.world.maxPositionIterations = 1;
-        console.log(this.world);
 
         this.bodies = [];
         this.colliders = [];
-        this.joints = [];
+        // this.joints = [];
 
         this.addPendulum();
 
+        console.log(this.world);
         this.future(100, this.step);
     }
 
@@ -42,23 +68,27 @@ ISSUES:
         const pivotDesc = new RAPIER.RigidBodyDesc('static');
         pivotDesc.setTranslation(0, 100, 0);
         const pivot = this.world.createRigidBody(pivotDesc);
+        pivot.world = this.world; // HACK to give serializer access to world
         this.bodies.push(pivot);
 
         const bobDesc = new RAPIER.RigidBodyDesc('dynamic');
         bobDesc.setTranslation(100, 100, 0);
         const bob = this.world.createRigidBody(bobDesc);
+        bob.world = this.world; // HACK to give serializer access to world
         this.bodies.push(bob);
 
         const colliderDesc = RAPIER.ColliderDesc.ball(5);
         colliderDesc.density = 1;
         const collider = bob.createCollider(colliderDesc);
+        collider.world = this.world; // HACK to give serializer access to world
         this.colliders.push(collider);
 
         const anchor1 = new RAPIER.Vector(0, 50, 0);
         const anchor2 = new RAPIER.Vector(0, 0, 0);
         const rodDesc = RAPIER.JointDesc.ball(anchor1, anchor2);
         const rod = this.world.createJoint(rodDesc, pivot, bob);
-        this.joints.push(rod);
+        rod.world = this.world; // HACK to give serializer access to world
+        // this.joints.push(rod);
     }
 
     step() {
