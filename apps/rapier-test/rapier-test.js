80ea33fe42562d62b8c38b953a82e819461fe7b0
Vanessa Freudenberg <vanessa@codefrau.net>
2020-08-07 12:02:12 -0700

    [rapier] add pendulum (no serialization yet)

--- apps/rapier-test/rapier-test.js
+++ apps/rapier-test/rapier-test.js
@@ -13,10 +13,57 @@ class RapierModel extends Model {
             },
         }
     }
+/*
 
+ISSUES:
+
+* separate objects refer to world, cannot be de-serialized separately (need ref to world)
+* Q: do we need to manual free() objects we retrieve from rapier?
+
+*/
     init() {
         this.world = new RAPIER.World(0.0, -9.81, 0.0);
+        this.world.maxVelocityIterations = 4;
+        this.world.maxPositionIterations = 1;
         console.log(this.world);
+
+        this.bodies = [];
+        this.colliders = [];
+        this.joints = [];
+
+        this.addPendulum();
+
+        this.future(100, this.step);
+    }
+
+    addPendulum() {
+        // a pendulum made from a pivot and bob connected by a rod
+
+        const pivotDesc = new RAPIER.RigidBodyDesc('static');
+        pivotDesc.setTranslation(0, 100, 0);
+        const pivot = this.world.createRigidBody(pivotDesc);
+        this.bodies.push(pivot);
+
+        const bobDesc = new RAPIER.RigidBodyDesc('dynamic');
+        bobDesc.setTranslation(100, 100, 0);
+        const bob = this.world.createRigidBody(bobDesc);
+        this.bodies.push(bob);
+
+        const colliderDesc = RAPIER.ColliderDesc.ball(5);
+        colliderDesc.density = 1;
+        const collider = bob.createCollider(colliderDesc);
+        this.colliders.push(collider);
+
+        const anchor1 = new RAPIER.Vector(0, 50, 0);
+        const anchor2 = new RAPIER.Vector(0, 0, 0);
+        const rodDesc = RAPIER.JointDesc.ball(anchor1, anchor2);
+        const rod = this.world.createJoint(rodDesc, pivot, bob);
+        this.joints.push(rod);
+    }
+
+    step() {
+        this.world.step();
+        //this.future(100, this.step);
     }
 }
 RapierModel.register("RapierModel");
