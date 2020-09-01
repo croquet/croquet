8dab3e4afe16a7dcd9323095111c62bcc21e0e60
Vanessa Freudenberg <vanessa@codefrau.net>
2020-08-31 17:49:31 -0700

    [sdk] update deploy.sh

--- historic/sdk/deploy.sh
+++ historic/sdk/deploy.sh
@@ -64,13 +64,8 @@ DOCS=$SDK/docs
 SRC_PKG=../libraries/packages/croquet
 
 if [ "$WHAT" != "docs" ] ; then
-    # update @croquet/croquet package
-    (cd $SRC_PKG ; npm run prepare)
-
-    if grep + $SRC_PKG/.env ; then
-        echo "ERROR: Version name looks not like a release!"
-        exit 1
-    fi
+    # cleanly checkout @croquet/croquet package
+    (cd $SRC_PKG ; npm run clean)
 
     # build & deploy library
     npx parcel build --public-url . --global Croquet -d $SDK -o croquet-$VERSION.min.js sdk.js
