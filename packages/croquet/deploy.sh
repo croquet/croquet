82091ab09593e2725c35e5586af5d46672f41558
Vanessa Freudenberg <vanessa@codefrau.net>
2020-08-31 17:48:56 -0700

    [teatime] remove .env after deploying

--- packages/croquet/deploy.sh
+++ packages/croquet/deploy.sh
@@ -3,6 +3,7 @@ cd `dirname "$0"`
 rm -rf pub/
 npm run build-prod
 source .env
+rm .env
 git update-index --no-assume-unchanged pub/croquet-croquet.js
 git commit -m "[teatime] croquet-croquet.js $CROQUET_VERSION" pub/croquet-croquet.js
 git update-index --assume-unchanged pub/croquet-croquet.js
