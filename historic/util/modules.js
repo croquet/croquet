e04f39d3c4a1ac95dd285f382ba6e0439bf20ea3
Vanessa Freudenberg <vanessa@codefrau.net>
2020-09-23 15:04:26 -0700

    [teatime] no more parcel magic for hashing

--- historic/util/modules.js
+++ historic/util/modules.js
@@ -2,31 +2,6 @@ import stableStringify from "fast-json-stable-stringify";
 import urlOptions from "./urlOptions";
 import { getUser } from "./user";
 
-/*
-We use the Parcel module system to inspect our own source code:
-
-module.bundle.modules = {
-    id: [<Module>, <Imports>],
-}
-
-<Module> = function(require,module,exports) {
-    ... module code (as pre-processed by Parcel)...
-}
-
-<Imports> = {
-    path1: id1,
-    path2: id2,
-}
-
-where "Module" is the module source code wrapped in a function definition
-and "Imports" is a dictionary mapping import paths to other module IDs.
-
-A minor complication is that module IDs look like file paths in
-development build, but are replaced by random short identifiers in
-production. That's why we must not ascribe any meaning to those IDs.
-*/
-
-
 // croquet.io and pi.croquet.io provide file servers themselves
 // everything else uses croquet.io via CORS
 export const CROQUET_HOST = window.location.hostname.endsWith("croquet.io") ? window.location.host : "croquet.io";
@@ -43,29 +18,6 @@ export function baseUrl(what='code') {
     const dev = urlOptions.has("dev", "host", "localhost"); // true on localhost or ngrok
     const host = dev ? `dev/${getUser("name", "GUEST")}/` : '';
     return `${fileServer()}/${host}${what}/`;
-}
-
-function allModules() {
-    return module.bundle ? module.bundle.modules : [];
-}
-
-function allModuleIDs() {
-    // ignore parcel runtime which is only used in dev builds and
-    // changes constantly (because it contains a dynamic port number)
-    return Object.keys(allModules()).filter(id => !id.endsWith('hmr-runtime.js'));
-}
-
-/**
- * find the given module
- * @param {String} id module ID
- * @returns {[Function, Array<String>]} the module function and its list of imports
- */
-function moduleWithID(id) {
-    return allModules()[id];
-}
-
-function _resolveImport(id, name) {
-    return moduleWithID(id)[1][name] || name;
 }
 
 function functionSource(fn) {
@@ -112,41 +64,6 @@ function classSrc(cls) {
     // session IDs.
 }
 
-/**
- * find source code of a given module (mangled by parcel.js)
- * @param {String} mod module name
- * @returns {String} the module source code
- */
-function sourceCodeOf(mod) {
-    const source = functionSource(moduleWithID(mod)[0]);
-
-    /*
-    // verify that code survives stringification
-    const fn = new Function('require', 'module', 'exports', source);
-    const src = functionSource(fn);
-    if (src !== source) throw Error("source does not match");
-    */
-
-    return source;
-}
-
-/** find all import names and IDs that are directly imported by a given module */
-function namedImportsOf(mod) {
-    return moduleWithID(mod)[1];
-}
-
-
-/** return all module IDs that are directly imported by a given module */
-function importsOf(mod) {
-    return Object.values(namedImportsOf(mod));
-}
-
-/** find all modules that directly import a given module */
-function _allImportersOf(mod) {
-    return allModuleIDs().filter(m => importsOf(m).includes(mod));
-}
-
-
 export function fromBase64url(base64) {
     return new Uint8Array(atob(base64.padEnd((base64.length + 3) & ~3, "=")
         .replace(/-/g, "+")
@@ -175,23 +92,6 @@ export async function hashBuffer(buffer) {
 function debugHashing() { return urlOptions.has("debug", "hashing", false); }
 
 const debugHashes = {};
-const debugModuleNames = { was_initialized: false };
-
-function resolveDebugModuleNames() {
-    for (const m of allModuleIDs()) {
-        for (const [name, id] of Object.entries(namedImportsOf(m))) {
-            const existing = debugModuleNames[id] || '';
-            const clean = name.replace(/^[./]*/, '');
-            if (clean.length > existing.length) debugModuleNames[id] = clean;
-        }
-    }
-    debugModuleNames.was_initialized = true;
-}
-
-function debugNameOf(mod) {
-    if (!debugModuleNames.was_initialized) resolveDebugModuleNames();
-    return debugModuleNames[mod] || mod;
-}
 
 const encoder = new TextEncoder();
 
@@ -202,22 +102,12 @@ export async function hashString(string) {
     return hash;
 }
 
-const fileHashes = {};
-
-export async function hashFile(mod) {
-    if (fileHashes[mod]) return fileHashes[mod];
-    const source = sourceCodeOf(mod).replace(/\s+/g, ' '); // a side effect of parcel is some whitespace mangling that can be different on different platforms.  hash on a version in which each run of whitespace is converted to a single space.
-    const hash = await hashString(source);
-    if (debugHashing()) debugHashes[hash].name = `Module ${debugNameOf(mod)}`;
-    return fileHashes[mod] = hash;
-}
-
-const extraHashes = [];
+const hashPromises = [];
 
 export function addClassHash(cls, classId) {
     const source = classSrc(cls);
     const hashPromise = hashString(source);
-    extraHashes.push(hashPromise);
+    hashPromises.push(hashPromise);
     if (debugHashing()) hashPromise.then(hash => {
         // console.log(`hashing model class ${classId}: ${hash}`);
         debugHashes[hash].name = `Class ${classId}`;
@@ -236,24 +126,12 @@ export function addConstantsHash(constants) {
         console.log(`hashing Croquet.Constants(${Object.keys(obj).join(', ')}): ${hash}`);
         if (debugHashing()) debugHashes[hash].name = "Croquet Constants";
     });
-    extraHashes.push(hashPromise);
+    hashPromises.push(hashPromise);
 }
 
 export async function hashSessionAndCode(name, options, sdk_version) {
-    // if the application is not using parcel to bundle, we will not see any modules
-    // but only the registered models. So when building the npm in production mode
-    // we do not even look at the parcel modules.
-    const production = process.env.NODE_ENV === "production";
-    const mods = production ? [] : allModuleIDs().filter(id => {
-        // we don't want to be encoding any package.json, because it includes a build-specific path name.  ar.js also causes trouble, for some as yet unknown reason.
-        // @@ this could be switched on or off under control of a process.env setting.
-        const exclude = id.endsWith("/package.json") || id.endsWith("/ar.js");
-        if (exclude) console.warn(`excluding ${id} from code hash`);
-        return !exclude;
-        }).sort();
-    // codeHashes are from registered user models and constants (in extraHashes)
-    // and possibly other modules if built in dev mode with parcel as mentioned above
-    const codeHashes = await Promise.all([...mods.map(hashFile), ...extraHashes]);
+    // codeHashes are from registered user models and constants (in hashPromises)
+    const codeHashes = await Promise.all(hashPromises);
     /** identifies the code being executed - user code, constants, SDK */
     const codeHash = await hashString([sdk_version, ...codeHashes].join('|'));
     /** identifies the session - not yet true unless name or options are unique */
