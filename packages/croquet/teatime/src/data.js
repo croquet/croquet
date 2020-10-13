0b458d4868717f637fd7e670c3298d54adc26d33
Vanessa Freudenberg <vanessa@codefrau.net>
2020-10-12 20:00:56 -0700

    [teatime] encrypt data assets

--- packages/croquet/teatime/src/data.js
+++ packages/croquet/teatime/src/data.js
@@ -1,43 +1,104 @@
-import { hashBuffer } from "@croquet/util/modules";
+import AES from "crypto-js/aes";
+import HmacSHA256 from "crypto-js/hmac-sha256";
+import WordArray from "crypto-js/lib-typedarrays";
+import Base64 from "crypto-js/enc-base64";
+
+import { hashString } from "@croquet/util/modules";
 import { App } from "@croquet/util/html";
 import urlOptions from "@croquet/util/urlOptions";
 import Island from "./island";
 
 
+const VERSION = '0';
+
 const DATAHANDLE_HASH = Symbol("hash");
+const DATAHANDLE_KEY = Symbol("key");
 
 const HandleCache = new Map();      // map hash => handle
-
-// TODO: encryption
-function getSessionKey(_sessionId) { return null; }
-async function encrypt(_key, data) { return data; }
-async function decrypt(_key, encrypted) { return encrypted; }
 
 function debug(what) {
     return urlOptions.has("debug", what, false);
 }
 
-function dataUrl(_sessionId, hash) {
-    // I thought we could use sessionId as part of the path for very easy tracking
-    // but then transferring the data to another sessionId (see issue #425) would be harder
+function dataUrl(hash) {
     return `https://croquet.io/files/v1/sessiondata/${hash}`;
 }
 
 async function hashData(data) {
-    return hashBuffer(data);
+    return hashString(data);
+}
+
+async function randomKey() {
+    return WordArray.random(32).toString(Base64);
+}
+
+// string, arraybuffer => string
+async function encrypt(keyBase64, data) {
+    const start = Date.now();
+    const plaintext = WordArray.create(data);
+    const key = Base64.parse(keyBase64);
+    const hmac = HmacSHA256(plaintext, key);
+    const iv = WordArray.random(16);
+    const { ciphertext } = AES.encrypt(plaintext, key, { iv });
+    const encrypted = "CRQ0" + [iv, hmac, ciphertext].map(wordArray => wordArray.toString(Base64)).join('');
+    if (debug("data")) console.log(`Croquet.Data: encryption (${data.byteLength} bytes) took ${Math.ceil(Date.now() - start)}ms`);
+    return encrypted;
+}
+
+// string, string => arraybuffer
+async function decrypt(keyBase64, encrypted) {
+    const start = Date.now();
+    const key = Base64.parse(keyBase64);
+    const version = encrypted.slice(0, 4);
+    const iv = Base64.parse(encrypted.slice(4, 4 + 24));
+    const mac = Base64.parse(encrypted.slice(4 + 24, 4 + 24 + 44));
+    const ciphertext = encrypted.slice(4 + 24 + 44);
+    const decrypted = AES.decrypt(ciphertext, key, { iv });
+    decrypted.clamp(); // clamping manually because of bug in HmacSHA256
+    const hmac = HmacSHA256(decrypted, key);
+    if (!compareHmacs(mac.words, hmac.words)) console.warn("decryption hmac mismatch"); // ¯\_(ツ)_/¯
+    const result = cryptoJsWordArrayToUint8Array(decrypted);
+    if (debug("data")) console.log(`Croquet.Data: decryption (${result.length} bytes) took ${Math.ceil(Date.now() - start)}ms`);
+    return result.buffer;
+}
+
+function compareHmacs(fst, snd) {
+    let ret = fst.length === snd.length;
+    for (let i=0; i<fst.length; i++) {
+        if (!(fst[i] === snd[i])) {
+            ret = false;
+        }
+    }
+    return ret;
+}
+
+function cryptoJsWordArrayToUint8Array(wordArray) {
+    const l = wordArray.sigBytes;
+    const words = wordArray.words;
+    const result = new Uint8Array(l);
+    let i = 0, j = 0;
+    while (true) {
+        if (i === l) break;
+        const w = words[j++];
+        result[i++] = (w & 0xff000000) >>> 24; if (i === l) break;
+        result[i++] = (w & 0x00ff0000) >>> 16; if (i === l) break;
+        result[i++] = (w & 0x0000ff00) >>> 8;  if (i === l) break;
+        result[i++] = (w & 0x000000ff);
+    }
+    return result;
 }
 
 async function upload(url, data) {
-    if (debug("data")) console.log(`Uploading ${data.byteLength} bytes to ${url}`);
+    if (debug("data")) console.log(`Croquet.Data: Uploading ${data.length} bytes to ${url}`);
     const response = await fetch(url, { method: 'PUT', referrer: App.referrerURL(), body: data});
     if (!response.ok) throw Error(`Croquet.Data: failed to upload ${url} (${response.status} ${response.statusText})`);
-    if (debug("data")) console.log(`Croquet.Data: uploaded (${response.status} ${response.statusText}) ${data.byteLength} bytes to ${url}`);
+    if (debug("data")) console.log(`Croquet.Data: uploaded (${response.status} ${response.statusText}) ${data.length} bytes to ${url}`);
 }
 
 async function download(url) {
     if (debug("data")) console.log(`Croquet.Data: Downloading from ${url}`);
     const response = await fetch(url, { referrer: App.referrerURL() });
-    if (response.ok) return response.arrayBuffer();
+    if (response.ok) return response.text();
     throw Error(`Croquet.Data: failed to download ${url} (${response.status} ${response.statusText})`);
 }
 
@@ -45,18 +106,22 @@ async function download(url) {
 export default class DataHandle {
     /**
      * Store data and return an (opaque) handle.
-     * @param {String} sessionId the sessionId for encryption
      * @param {ArrayBuffer} data the data to be stored
      * @param {Boolean} doNotWait if true, return before storing finished and resolve `handle.stored` when done
      * @returns {Promise<DataHandle>} return promise for the handle. If requested, `handle.stored` will be another promise that resolves when uploading is done.
      */
-    static async store(sessionId, data, doNotWait=false) {
+    static async store(data, doNotWait=false, thirdArg=false) {
+        if (typeof data === "string" && typeof doNotWait === "object") {
+            console.warn("Deprecated: Croquet.Data.store(data) called with sessionID")
+            data = doNotWait;
+            doNotWait = thirdArg;
+        }
         if (Island.hasCurrent()) throw Error("Croquet.Data.store() called from Model code");
-        const key = getSessionKey(sessionId);
+        const key = await randomKey();
         const encrypted = await encrypt(key, data);
         const hash = await hashData(encrypted);
-        const handle = new DataHandle(hash);
-        const url = dataUrl(sessionId, hash);
+        const handle = new DataHandle(hash, key);
+        const url = dataUrl(hash);
         const promise = upload(url, encrypted);
         // if we uploaded the same file in this same session before, then the promise already exists
         if (!handle.stored) {
@@ -75,31 +140,38 @@ export default class DataHandle {
 
     /**
      * Fetch data for a given data handle
-     * @param {String} sessionId the sessionId for decryption
      * @param {DataHandle} handle created by {@link Data.store}
      * @returns {Promise<ArrayBuffer>} the data
      */
-    static async fetch(sessionId, handle) {
+    static async fetch(handle, secondArg) {
+        if (typeof handle === "string" && typeof secondArg === "object") {
+            console.warn("Deprecated: Croquet.Data.fetch(handle) called with sessionID")
+            handle = secondArg;
+        }
         if (Island.hasCurrent()) throw Error("Croquet.Data.fetch() called from Model code");
         const hash = handle && handle[DATAHANDLE_HASH];
-        if (typeof hash !== "string") throw Error("Croquet.Data.fetch() called with invalid handle");
-        const url = dataUrl(sessionId, hash);
+        const key = handle && handle[DATAHANDLE_KEY];
+        if (typeof hash !== "string" ||typeof key !== "string") throw Error("Croquet.Data.fetch() called with invalid handle");
+        const url = dataUrl(hash);
         const encrypted = await download(url);
-        const key = getSessionKey(sessionId);
         return decrypt(key, encrypted);
     }
 
     /** @private */
     static fromId(id) {
-        return new this(id);
+        const version = id.slice(0, 1);
+        if (version !== VERSION) throw Error(`Croquet.Data expected handle v${VERSION} got v${version}`);
+        const hash = id.slice(1, 1 + 43);
+        const key = id.slice(1 + 43);
+        return new this(hash, key);
     }
 
     /** @private */
     static toId(handle) {
-        return handle && handle[DATAHANDLE_HASH];
+        return handle && `${VERSION}${handle[DATAHANDLE_HASH]}${handle[DATAHANDLE_KEY]}`;
     }
 
-    constructor(hash) {
+    constructor(hash, key) {
         const existing = HandleCache.get(hash);
         if (existing) {
             if (debug("data")) console.log(`Croquet.Data: using cached handle for ${hash}`);
@@ -107,6 +179,7 @@ export default class DataHandle {
         }
         // stored under Symbol key to be invisible to user code
         Object.defineProperty(this, DATAHANDLE_HASH, { value: hash });
+        Object.defineProperty(this, DATAHANDLE_KEY, { value: key });
         HandleCache.set(hash, this);
         if (debug("data")) console.log(`Croquet.Data: created new handle for ${hash}`);
     }
@@ -116,6 +189,6 @@ export default class DataHandle {
 
 export const DataHandleSpec = {
     cls: DataHandle,
-    write: handle => handle[DATAHANDLE_HASH],
-    read: state => new DataHandle(state),
+    write: handle => DataHandle.toId(handle),
+    read: state => DataHandle.fromId(state),
 };
