c294acab157cb88ede8cfa573d32b9fbf93dc7ce
Vanessa Freudenberg <vanessa@codefrau.net>
2020-10-19 18:39:46 -0700

    [sdk] remove dist/croquet-dev-bundled.js build

--- historic/sdk/deploy.sh
+++ historic/sdk/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # to redeploy docs only: deploy.sh docs
-# to deploy docs and sdk: deploy.sh release
-# to deploy prerelease sdk: deploy.sh prerelease
+# to deploy docs release: deploy.sh release
 
 cd `dirname "$0"`
 
@@ -21,18 +20,6 @@ esac
 
 case "$WHAT" in
 docs)
-    MSG="docs $RELEASEVERSION"
-    BUILDDOCS=true
-    ;;
-prerelease)
-    if ! $PRERELEASE ; then
-        echo "$VERSION does not look like a pre-release version!"
-        exit 0
-    fi
-    npm version "$VERSION"
-    [ $? -ne 0 ] && exit 1
-    MSG="prerelease $VERSION"
-    BUILDDOCS=false
     ;;
 release)
     if $PRERELEASE ; then
@@ -45,13 +32,10 @@ release)
     fi
     npm version "$VERSION"
     [ $? -ne 0 ] && exit 1
-    MSG="sdk+docs $VERSION"
-    PRERELEASE=false
-    BUILDDOCS=true
     ;;
 *)
     DEPLOY=`basename $0`
-    echo "Usage: $DEPLOY (release|prerelease|docs)"
+    echo "Usage: $DEPLOY (release|docs)"
     exit 1
 esac
 
@@ -60,39 +44,22 @@ echo "DEPLOYING $MSG"
 DIR=../../servers/croquet-io-testing
 SDK=$DIR/sdk
 DOCS=$SDK/docs
-SRC_PKG=../libraries/packages/croquet
 
-if [ "$WHAT" != "docs" ] ; then
-    # cleanly checkout @croquet/croquet package
-    (cd $SRC_PKG ; npm run clean)
+rm -rf build/*
+npx jsdoc -c jsdoc.json -d build || exit
+sed -i '' "s/@CROQUET_VERSION@/$RELEASEVERSION/" build/*.html || exit
 
-    # build & deploy library
-    NODE_ENV=production npx rollup -c rollup.config.js -o $SDK/croquet-$VERSION.min.js || exit 1
-
-    # always link as latest-pre
-    (cd $SDK; ln -sf croquet-$VERSION.min.js croquet-latest-pre.min.js; echo $VERSION > croquet-latest-pre.txt)
-    # link as latest unless prerelease
-    $PRERELEASE || (cd $SDK; ln -sf croquet-$VERSION.min.js croquet-latest.min.js; echo $VERSION > croquet-latest.txt)
-fi
-
-# build docs unless pre-release
-if $BUILDDOCS ; then
-    rm -r build/*
-    npx jsdoc -c jsdoc.json -d build || exit
-    sed -i '' "s/@CROQUET_VERSION@/$RELEASEVERSION/" build/*.html || exit
-
-    # deploy docs
-    rm -r $DOCS/*
-    # (fake conduct.html to fool parcel)
-    touch build/conduct.html
-    npx parcel build --public-url . --no-source-maps -d $DOCS build/*.html || exit
-    # (remove fake conduct.html and substitute proper link)
-    rm $DOCS/conduct.html
-    sed -i '' "s|conduct.html|/conduct.html|" $DOCS/index.html
-fi
+# clean old docs
+rm -r $DOCS/*
+# (fake conduct.html to fool parcel)
+touch build/conduct.html
+npx parcel build --public-url . --no-source-maps -d $DOCS build/*.html || exit
+# (remove fake conduct.html and substitute proper link)
+rm $DOCS/conduct.html
+sed -i '' "s|conduct.html|/conduct.html|" $DOCS/index.html
 
 git add -A $SDK/ package.json
-git commit -m "[sdk] deploy $MSG to croquet.io/testing" $SDK/ package.json || exit
+git commit -m "[sdk] deploy docs $RELEASEVERSION to croquet.io/testing" $SDK/ package.json || exit
 
 git --no-pager show --stat
 
