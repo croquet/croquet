4602c2fc1efa33c3df57ea23cd375a49724fbe14
Vanessa Freudenberg <vanessa@codefrau.net>
2021-03-11 16:31:44 -0800

    [teatime] more debug logging

--- packages/croquet/teatime/src/controller.js
+++ packages/croquet/teatime/src/controller.js
@@ -662,13 +662,15 @@ export default class Controller {
                     const syncNewest = seq;
                     const syncOldest = snapshotSeq !== undefined ? snapshotSeq
                         : messages.length > 0 ? messages[0][1] : syncNewest;
+                    if (DEBUG.messages) console.log(this.id, `rejoin: we have #${ourOldest}-#${ourNewest} SYNC has #${syncOldest}-#${syncNewest}`);
                     const seamlessRejoin = sameSession        //
                         && inSequence(syncOldest, ourNewest)   // there must be no gap between our last message and the first synced message
                         && inSequence(ourOldest, syncNewest);  // the reflector state must not be older than our island
                     if (seamlessRejoin) {
-                        // rejoin is safe, just drop duplicate messages
+                        // rejoin is safe, just discard duplicate messages
                         if (messages[0] && inSequence(messages[0][1], ourNewest)) {
                             const discard = 1 + (ourNewest - messages[0][1]) | 0; // signed difference (!)
+                            if (DEBUG.messages) console.log(this.id, `rejoin: discarding ${discard} messages #${messages[0][1]}-#${ourNewest}`);
                             messages.splice(0, discard);
                         }
                         // proceed to enqueue the messages we missed while disconnected
