d695b4355c3fdecf791d4b51c31232a6d8965e71
Vanessa Freudenberg <vanessa@codefrau.net>
2021-03-22 19:09:55 -0700

    [pix] switch to rollup-plugin-styles

--- apps/pix/rollup.config.js
+++ apps/pix/rollup.config.js
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs'
 import license from 'rollup-plugin-license';
 import sourcemaps from 'rollup-plugin-sourcemaps';
-import css from 'rollup-plugin-css-only';
+import styles from "rollup-plugin-styles";
 import { terser } from 'rollup-plugin-terser';
 
 const is_dev_build = process.env.BUILD !== 'production';
@@ -20,7 +20,7 @@ export default {
     plugins: [
         resolve(), // tells Rollup how to resolve stuff in node_modules
         commonjs(), // converts all modules (including @croquet/croquet) to ES modules
-        css({ output: 'pix-bundled.css' }),
+        styles(),
         sourcemaps(), // use sourcemaps from source files
         !is_dev_build && terser({
             mangle: {module: true},
