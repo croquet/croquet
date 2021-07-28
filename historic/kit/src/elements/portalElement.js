bafe47ebf9ce3e46ee831cc52e304ecda30bb452
Vanessa Freudenberg <vanessa@codefrau.net>
2021-07-28 16:55:14 -0700

    [kit] remove obsolete code

--- historic/kit/src/elements/portalElement.js
+++
@@ -1,4 +0,0 @@
-import PortalPart from "../modelParts/portal";
-
-// PortalPart can directly be used as an Element, since it has a SpatialPart
-export default PortalPart;
--- historic/kit/src/util/userid.js
+++
@@ -1,5 +0,0 @@
-let id = Array.from(crypto.getRandomValues(new Uint8Array(3)))
-    .map(e => e.toString(16).padStart(2, '0'))
-    .join('');
-
-export const userID = {id, color: id};
