8df0054ef21c795f2b972be807c5600313aab3d9
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2021-07-30 16:28:58 -0700

    [sdk] add sdk-pre to the list of files to be commited

--- packages/croquet/deploy.sh
+++ packages/croquet/deploy.sh
@@ -25,6 +25,7 @@ esac
 
 # publish pub/croquet.min.js to croquet.io/dev/sdk
 SDK=../../../../servers/croquet-io-dev/sdk
+SDKPRE=../../../../servers/croquet-io-dev/sdk-pre
 sed 's,//# sourceMappingURL.*,,' pub/croquet.min.js > $SDK/croquet-$VERSION.min.js
 $PRERELEASE && cat prerelease.js >> $SDK/croquet-$VERSION.min.js
 cp pub/croquet.min.js.map $SDK/croquet-$VERSION.min.js.map
@@ -45,8 +46,8 @@ fi
 # commit
 FILES="cjs/croquet-croquet.js cjs/croquet-croquet.js.map pub/croquet.min.js pub/croquet.min.js.map"
 git update-index --no-assume-unchanged $FILES
-git add -A cjs/ pub/ $SDK/
-git commit -m "[teatime] deploy $VERSION" cjs/ pub/ $SDK/ || exit
+git add -A cjs/ pub/ $SDK/ $SDKPRE/
+git commit -m "[teatime] deploy $VERSION" cjs/ pub/ $SDK/ $SDKPRE|| exit
 git update-index --assume-unchanged $FILES
 git --no-pager show --stat
 
