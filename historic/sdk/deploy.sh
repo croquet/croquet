1de9d2e5214a6764971d6af478b0866c6df6fcc9
Aran Lunzer <aran@acm.org>
2021-07-30 00:12:19 -0700

    [docs] script redirect needs to know the starting directory

--- historic/sdk/deploy.sh
+++ historic/sdk/deploy.sh
@@ -3,4 +3,6 @@
 # to update docs for prev release: deploy.sh docs
 # to deploy docs for pre-release: deploy.sh prerelease
 
-../docs/deploy.sh "$@"
+# https://stackoverflow.com/questions/59895/how-can-i-get-the-source-directory-of-a-bash-script-from-within-the-script-itsel
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+$DIR/../docs/deploy.sh "$@"
