b3ad48b5c362cf0e03af8a12550b8710e5dc7839
Vanessa Freudenberg <vanessa@codefrau.net>
2021-08-23 20:52:58 -0700

    [docs] deploy scripts

--- historic/sdk/deploy.sh
+++ historic/sdk/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # to deploy docs for new release: deploy.sh release
-# to update docs for prev release: deploy.sh docs
 # to deploy docs for pre-release: deploy.sh prerelease
 
-# https://stackoverflow.com/questions/59895/how-can-i-get-the-source-directory-of-a-bash-script-from-within-the-script-itsel
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+DIR=`dirname "$0"`
 $DIR/../docs/deploy.sh "$@"
