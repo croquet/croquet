4f845e4cf09d8b9af5629451780afadb58b6d72e
Vanessa Freudenberg <vanessa@codefrau.net>
2021-09-14 13:51:54 -0700

    [teatime] update prerelease version notice

--- packages/croquet/prerelease.js
+++ packages/croquet/prerelease.js
@@ -3,5 +3,5 @@ if (!location.hostname || Croquet.App.isCroquetHost(location.hostname)) {
     console.warn('This pre-release of Croquet is for internal use only!');
 } else {
     console.error('This pre-release of Croquet is for internal use only!');
-    console.log('Please use <script src="https://unpkg.com/@croquet/croquet@0.5"></script>');
+    console.log('Please use <script src="https://unpkg.com/@croquet/croquet@1.0"></script>');
 }
