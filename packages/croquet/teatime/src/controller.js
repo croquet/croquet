e2f8c5032ba4755fc6ae827a39d7c680dbd02843
Vanessa Freudenberg <vanessa@codefrau.net>
2022-10-31 15:02:27 -0700

    [api] fix regexes to allow IPv6

--- packages/croquet/teatime/src/controller.js
+++ packages/croquet/teatime/src/controller.js
@@ -95,35 +95,38 @@ function getBackendUrls() {
 
 /*
 function isLocalUrl(hostname) {
-    const IP_RANGES = [
+    // copied from devauth/signurl/urlMatcher.js
+
+    const LOCAL_PATTERNS = [
+        /localhost/,
+        /.*\.local/,
+        /.*\.ngrok.io/,
         // 10.0.0.0 - 10.255.255.255
-        /^(:{2}f{4}:)?10(?:\.\d{1,3}){3}$/,
+        /10(?:\.\d{1,3}){3}/,
         // 127.0.0.0 - 127.255.255.255
-        /^(:{2}f{4}:)?127(?:\.\d{1,3}){3}$/,
+        /127(?:\.\d{1,3}){3}/,
         // 169.254.1.0 - 169.254.254.255
-        /^(::f{4}:)?169\.254\.([1-9]|1?\d\d|2[0-4]\d|25[0-4])\.\d{1,3}$/,
+        /169\.254\.([1-9]|1?\d\d|2[0-4]\d|25[0-4])\.\d{1,3}/,
         // 172.16.0.0 - 172.31.255.255
-        /^(:{2}f{4}:)?(172\.1[6-9]|172\.2\d|172\.3[01])(?:\.\d{1,3}){2}$/,
+        /(172\.1[6-9]|172\.2\d|172\.3[01])(?:\.\d{1,3}){2}/,
         // 192.168.0.0 - 192.168.255.255
-        /^(:{2}f{4}:)?192\.168(?:\.\d{1,3}){2}$/,
+        /192\.168(?:\.\d{1,3}){2}/,
         // fc00::/7
-        /^f[cd][\da-f]{2}(::1$|:[\da-f]{1,4}){1,7}$/,
+        /\[f[cd][\da-f]{2}(:?:[\da-f]{1,4}){1,7}\]/,
         // fe80::/10
-        /^fe[89ab][\da-f](::1$|:[\da-f]{1,4}){1,7}$/,
+        /\[fe[89ab][\da-f](:?:[\da-f]{1,4}){1,7}\]/,
         // ::1
-        /^::1$/,
+        /\[::1\]/,
+        // ::ffff:7f00:1
+        /\[::ffff:7f00:1\]/,
     ];
 
     // Concat all RegExes from above into one
-    const IP_TESTER_RE = new RegExp(
-        `^(${IP_RANGES.map(re => re.source).join('|')})$`,
+    const LOCAL_PATTERNS_RE = new RegExp(
+        `^(${LOCAL_PATTERNS.map(re => re.source).join('|')})$`,
     );
 
-    if (hostname === 'localhost') {
-        return true;
-    }
-
-    const isLocal =  IP_TESTER_RE.test(hostname);
+    const isLocal = LOCAL_PATTERNS_RE.test(hostname);
 
     return isLocal;
 }
