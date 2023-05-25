21ef59d80fc55fdcab091a96b4844f43575ff97a
Vanessa Freudenberg <vanessa@codefrau.net>
2023-05-25 13:59:42 -0700

    [croquet-in-a-box] allow port on command line

--- server/croquet-in-a-box/croquet-in-a-box.sh
+++ server/croquet-in-a-box/croquet-in-a-box.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
+# usage: ./croquet-in-a-box.sh [port]
+
 cd $(dirname "$0")
 
+# these are used inside docker-compose.yml
+export HOST_PORT=${1:-8000}
+export WEB_ROOT_PATH=$WONDERLAND/servers/croquet-io-dev
+export REFLECTOR_LABEL=`hostname`
+
 # figure out IP address, or use localhost
-IP=`ifconfig | grep "inet .* broadcast" | head -1 | awk '{print $2}'`
-if [[ -z "$IP" ]] ; then
-    IP=localhost
+# this mighht be specific to MacOS, will need to do check on Linux/Windows
+HOST_IP=`ifconfig | grep "inet .* broadcast" | head -1 | awk '{print $2}'`
+if [[ -z "$HOST_IP" ]] ; then
+    HOST_IP=localhost
 fi
 
-echo "web: http://$IP:8000"
-echo "files: http://$IP:8000/files"
-echo "reflector: http://$IP:8000/reflector"
+echo "web: http://$HOST_IP:$HOST_PORT"
+echo "files: http://$HOST_IP:$HOST_PORT/files"
+echo "reflector: http://$HOST_IP:$HOST_PORT/reflector"
 echo
-echo "example: http://$IP:8000/guardians/?reflector=ws://$IP:8000/reflector&files=http://$IP:8000/files/"
+echo "example: http://$HOST_IP:$HOST_PORT/guardians/?reflector=ws://$HOST_IP:$HOST_PORT/reflector&files=http://$HOST_IP:$HOST_PORT/files"
 echo
 echo "Press Ctrl-C to stop"
 echo
@@ -22,7 +30,4 @@ REFLECTOR=$WONDERLAND/croquet/reflector
 $REFLECTOR/gen-obfuscated-docker.sh --storage=none --standalone --no-loglatency --no-logtime
 
 # run reflector and nginx as defined in docker-compose.yml
-export HOST_PORT=8000
-export WEB_ROOT_PATH=$WONDERLAND/servers/croquet-io-dev
-export REFLECTOR_LABEL=`hostname`
 docker compose up
