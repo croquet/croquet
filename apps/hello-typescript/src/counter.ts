157dbd94302df8103cb40ed2778d702c0becd5dc
Vanessa Freudenberg <vanessa@codefrau.net>
2024-08-15 20:21:31 -0700

    [hello-typescript] add Croquet

--- apps/hello-typescript/src/counter.ts
+++ apps/hello-typescript/src/counter.ts
@@ -1,9 +1,54 @@
-export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
-  const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
+import { Model, View, Session } from '@croquet/croquet'
+
+class Counter extends Model {
+
+  counter = 0
+
+  init() {
+      this.counter = 0;
+      this.subscribe("counter", "reset", this.resetCounter);
+      this.future(1000).tick();
   }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+
+  resetCounter() {
+      this.counter = 0;
+      this.publish("counter", "update");
+  }
+
+  tick() {
+      this.counter++;
+      this.publish("counter", "update");
+      this.future(1000).tick();
+  }
+
+}
+Counter.register("MyModel");
+
+
+export function setupCounter(element: HTMLButtonElement) {
+
+  class Button extends View {
+    model: Counter;
+
+    constructor(model: Counter) {
+        super(model);
+        this.model = model;
+        this.subscribe("counter", "update", this.updateCounter);
+        element.onclick = () => this.publish("counter", "reset");
+        this.updateCounter();
+    }
+
+    updateCounter() {
+        element.innerHTML = `${this.model.counter}`;
+    }
+  }
+
+  Session.join({
+    appId: "io.croquet.hello-typescript",
+    apiKey: "2DT9VCoCKtvXMKkBGZXNLrUEoZMn48ojXPC8XFAuuO",
+    name: location.origin + location.pathname, // one session per URL
+    password: "shared",
+    model: Counter,
+    view: Button,
+  })
 }
--- apps/hello-typescript/src/main.ts
+++ apps/hello-typescript/src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
+import typescriptLogo from '/typescript.svg'
 import viteLogo from '/vite.svg'
+import croquetLogo from '/croquet.png'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
@@ -11,12 +12,15 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <a href="https://croquet.io" target="_blank">
+      <img src="${croquetLogo}" class="logo" alt="Croquet logo" />
+    </a>
+    <h1>Vite + TypeScript + Croquet</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
+      Click on the Vite, TypeScript, and Croquet logos to learn more
     </p>
   </div>
 `
