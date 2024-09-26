78bf9ab457468ca58a7a56a66dc1c2391c252a56
Aran Lunzer <aran@acm.org>
2024-09-26 16:39:15 -0700

    bump synch version to 2.0.2

--- packages/reflector/reflector.js
+++ packages/reflector/reflector.js
@@ -2,7 +2,7 @@
 /* eslint-disable object-shorthand */
 /* eslint-disable prefer-arrow-callback */
 
-const SYNCH_VERSION = "2.0.1"
+const SYNCH_VERSION = "2.0.2"
 
 const os = require('node:os');
 const fs = require('node:fs');
