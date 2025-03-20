c84af025fed1a264d636f4ef0279fe3d2d31c4dc
Vanessa Freudenberg <vanessa@codefrau.net>
2025-03-20 07:54:03 -0700

    clean up dirs

--- historic/simpleapp/src/util/userid.js
+++
@@ -1,9 +0,0 @@
-const moduleVersion = module.bundle.v ? (module.bundle.v[module.id] || 0) + 1 : 0;
-if (module.bundle.v) { console.log(`Hot reload ${module.id}#${moduleVersion}`); module.bundle.v[module.id] = moduleVersion; }
-
-
-let id = Array.from(crypto.getRandomValues(new Uint8Array(3)))
-    .map(e => e.toString(16).padStart(2, '0'))
-    .join('');
-
-export const userID = {id, color: id};
--- packages/croquet-observable/README.md
+++
@@ -1,5 +0,0 @@
-# Croquet Observable
-
-_to be documented_
-
-In the mean time see croquet.io/docs
