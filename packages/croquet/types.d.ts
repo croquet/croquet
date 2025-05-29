dc306e302c04685af8970cbfe90adecbf59480f8
Vanessa Freudenberg <vanessa@codefrau.net>
2025-05-29 18:19:02 +0200

    [teatime] fix types for init()

--- packages/croquet/types.d.ts
+++ packages/croquet/types.d.ts
@@ -245,7 +245,7 @@ declare module "@croquet/croquet" {
          * @param options - there are no system-defined options, you're free to define your own
          * @public
          */
-        init(_options: any, persistentData?: any): void;
+        init(_options?: any, persistentData?: any): void;
 
         /**
          * Unsubscribes all [subscriptions]{@link Model#subscribe} this model has,
